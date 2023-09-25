31369939923181333b612d9f04daf8a796f98fac
chiarapascucci-gds <chiara.pascucci@digital.cabinet-office.gov.uk>
2023-09-25 17:46:04 +0100

    lambda writing to db

--- src/handlers/intervention-history-handler.ts
+++ src/handlers/intervention-history-handler.ts
@@ -1,6 +1,22 @@
 import type { Context, APIGatewayEvent } from 'aws-lambda';
+import { DynamoDBClient, PutItemCommand } from '@aws-sdk/client-dynamodb';
 
 export const handle = async (event: APIGatewayEvent, context: Context): Promise<void> => {
   console.log(event);
   console.log(context);
+  const client = new DynamoDBClient();
+  const command = new PutItemCommand({
+    TableName: 'ais-core-interventions-history',
+    Item: {
+      AccountId: {
+        S: 'testAccountID',
+      },
+      CreatedAt: {
+        N: Math.floor(Date.now() / 1000).toString(),
+      },
+    },
+  });
+
+  const response = await client.send(command);
+  console.log(response);
 };
