14647c506f74bc4e7a9fbea7c8a53505163b4654
Gardiner <karla.gardiner@digital.cabinet-office.gov.uk>
2023-10-19 18:50:01 +0100

    ATB-1113 fixed linting

--- src/services/app-config-service.ts
+++ src/services/app-config-service.ts
@@ -51,7 +51,7 @@ export class AppConfigService {
 
   public get endpoint() {
     const endpoint = this.validateConfiguration('END_POINT');
-    const prefix = endpoint.charAt(0) === '/' ? "" : '/';
+    const prefix = endpoint.charAt(0) === '/' ? '' : '/';
     return prefix + endpoint;
   }
 
