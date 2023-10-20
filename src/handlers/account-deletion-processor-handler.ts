ff144175a54bffafbf2eee951451fd2251334158
Jessica <jessica.li@digital.cabinet-office.gov.uk>
2023-10-20 16:41:06 +0100

    wip

--- src/handlers/account-deletion-processor-handler.ts
+++ src/handlers/account-deletion-processor-handler.ts
@@ -1,62 +1,54 @@
+import { DynamoDBClient, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
 import logger from '../commons/logger';
-import { DynamoDbService as DynamoDatabaseService } from '../services/dynamo-db-service';
 import { LOGS_PREFIX_SENSITIVE_INFO } from '../data-types/constants';
-import { AppConfigService } from '../services/app-config-service';
-import type { Context, SNSMessage, SQSEvent, SQSRecord } from 'aws-lambda';
-import { DeleteStatusUpdateSNSMessage } from '../data-types/interfaces';
+import { AppConfigService } from '../services/app-config-service' ;
+import type { Context, SQSEvent } from 'aws-lambda';
 
+const dynamodbClient = new DynamoDBClient();
 const appConfig = AppConfigService.getInstance();
-const ddbService = new DynamoDatabaseService(appConfig.tableName);
 
 export const handler = async (event: SQSEvent, context: Context): Promise<void> => {
   logger.addContext(context);
-  if (!event.Records[0] || event.Records.length === 0) {
-    logger.error('The event does not contain any records.');
-    return;
+  if (!event.Records[0] || event.Records.length === 0 ) {
+    logger.error("The event does not contain any records.");
+    throw new Error;
   }
 
-  const updateRecordsByIdPromises = event.Records.map((record) => {
-    const userId = getUserId(record);
-    return userId ? updateDeleteStatusId(userId) : undefined;
-  }).filter((prom) => prom !== undefined);
-  await Promise.all(updateRecordsByIdPromises);
-};
+  let pk: string;
 
-function getUserId(record: SQSRecord) {
-  let messageBody: SNSMessage;
-  let message: DeleteStatusUpdateSNSMessage;
   try {
-    messageBody = JSON.parse(record.body);
-    message = JSON.parse(messageBody.Message);
-  } catch {
-    logger.error('The SQS message can not be accessed.');
-    return;
-  }
-  if (!message.hasOwnProperty('user_id')) {
-    logger.warn('Attribute missing: user_id.');
-    return;
-  }
-
-  const userId = message.user_id;
-
-  if (typeof userId !== 'string') {
-    logger.warn('Attribute invalid: user_id is not a string.');
-    return;
-  }
-
-  if (userId.trim() === '') {
-    logger.warn('Attribute invalid: user_id is empty.');
-    return;
-  }
-  return userId.trim();
-}
-
-async function updateDeleteStatusId(userId: string) {
-  try {
-    await ddbService.updateDeleteStatus(userId);
-    logger.info(`${LOGS_PREFIX_SENSITIVE_INFO} Account ${userId} marked as deleted`);
+    const sqsMessage = JSON.parse(event.Records[0].body) as { pk : string };
+    pk = sqsMessage.pk;
   } catch (error) {
-    logger.error(`${LOGS_PREFIX_SENSITIVE_INFO} Error updating account ${userId}`, { error });
-    throw new Error('Failed to update the account status.');
+    logger.error("The SNS message can not be accessed.");
+    throw new Error;
   }
-}
+
+  const ttl = Date.now() + appConfig.maxRetentionSeconds * 1000;
+
+  const parameters = {
+    TableName: appConfig.tableName,
+    Key: {
+      pk: { S: pk },
+    },
+    UpdateExpression: 'SET #isAccountDeleted = :isAccountDeleted, #ttl = :ttl',
+    ExpressionAttributeNames: {
+      '#isAccountDeleted': 'isAccountDeleted',
+      '#ttl': 'ttl',
+    },
+    ExpressionAttributeValues: {
+      ':isAccountDeleted': { BOOL: true },
+      ':ttl': { N: ttl.toString() },
+    },
+    ConditionExpression: 'attribute_exists(pk)',
+  };
+
+  const command = new UpdateItemCommand(parameters);
+
+  try {
+    await dynamodbClient.send(command);
+    logger.info(`${LOGS_PREFIX_SENSITIVE_INFO} Account ${pk} marked as deleted`);
+  } catch (error) {
+    logger.error(`${LOGS_PREFIX_SENSITIVE_INFO} Error updating account ${pk}`, { error });
+  }
+};
