412dc6c2da7ebf04e70134baf6c2ec4d9784c8ed
Jessica <jessica.li@digital.cabinet-office.gov.uk>
2023-10-20 16:41:06 +0100

    wip

--- src/handlers/account-deletion-processor-handler.ts
+++ src/handlers/account-deletion-processor-handler.ts
@@ -2,12 +2,28 @@ import { DynamoDBClient, UpdateItemCommand } from '@aws-sdk/client-dynamodb';
 import logger from '../commons/logger';
 import { LOGS_PREFIX_SENSITIVE_INFO } from '../data-types/constants';
 import { AppConfigService } from '../services/app-config-service' ;
+import type { Context, SQSEvent } from 'aws-lambda';
 
 const dynamodbClient = new DynamoDBClient();
 const appConfig = AppConfigService.getInstance();
 
-export const handler = async (user_id: { pk: string }): Promise<void> => {
-  const pk = user_id.pk;
+export const handler = async (event: SQSEvent, context: Context): Promise<void> => {
+  logger.addContext(context);
+  if (!event.Records[0] || event.Records.length === 0 ) {
+    logger.error("The event does not contain any records.");
+    throw new Error;
+  }
+
+  let pk: string;
+
+  try {
+    const sqsMessage = JSON.parse(event.Records[0].body) as { pk : string };
+    pk = sqsMessage.pk;
+  } catch (error) {
+    logger.error("The SNS message can not be accessed.");
+    throw new Error;
+  }
+
   const ttl = Date.now() + appConfig.maxRetentionSeconds * 1000;
 
   const parameters = {
