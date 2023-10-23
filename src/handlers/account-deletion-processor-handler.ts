bec4bbe2f1762a67cb10a7a975943b9b87431db1
Jessica <jessica.li@digital.cabinet-office.gov.uk>
2023-10-23 16:56:16 +0100

    fixing yarn lint

--- src/handlers/account-deletion-processor-handler.ts
+++ src/handlers/account-deletion-processor-handler.ts
@@ -1,7 +1,7 @@
 import logger from '../commons/logger';
 import { DynamoDbService as DynamoDatabaseService } from '../services/dynamo-db-service';
 import { LOGS_PREFIX_SENSITIVE_INFO } from '../data-types/constants';
-import { AppConfigService } from '../services/app-config-service' ;
+import { AppConfigService } from '../services/app-config-service';
 import type { Context, SNSMessage, SQSEvent, SQSRecord } from 'aws-lambda';
 import { DeleteStatusUpdateSNSMessage } from '../data-types/interfaces';
 // import { logAndPublishMetric } from '../commons/metrics';
@@ -11,12 +11,12 @@ const ddbService = new DynamoDatabaseService(appConfig.tableName);
 
 export const handler = async (event: SQSEvent, context: Context): Promise<void> => {
   logger.addContext(context);
-  if (!event.Records[0] || event.Records.length === 0 ) {
-    logger.error("The event does not contain any records.");
+  if (!event.Records[0] || event.Records.length === 0) {
+    logger.error('The event does not contain any records.');
     return;
   }
 
-  const updateVCsByIdPromises = event.Records.map((record)=> {
+  const updateVCsByIdPromises = event.Records.map((record) => {
     const userId = getUserId(record);
     return userId ? updateDeleteStatusId(userId) : undefined;
   }).filter((prom) => prom !== undefined);
@@ -28,14 +28,13 @@ export const handler = async (event: SQSEvent, context: Context): Promise<void> 
 function getUserId(record: SQSRecord) {
   let messageBody: SNSMessage;
   let message: DeleteStatusUpdateSNSMessage;
-
-try {
-  messageBody = JSON.parse(record.body);
-  message = JSON.parse(messageBody.Message);
-} catch (error) {
-  logger.error("The SQS message can not be accessed.");
-  return;
-}
+  try {
+    messageBody = JSON.parse(record.body);
+    message = JSON.parse(messageBody.Message);
+  } catch {
+    logger.error('The SQS message can not be accessed.');
+    return;
+  }
   if (!message.hasOwnProperty('user_id')) {
     logger.warn('Attribute missing: user_id.');
     return;
@@ -61,10 +60,5 @@ async function updateDeleteStatusId(userId: string) {
     logger.info(`${LOGS_PREFIX_SENSITIVE_INFO} Account ${userId} marked as deleted`);
   } catch (error) {
     logger.error(`${LOGS_PREFIX_SENSITIVE_INFO} Error updating account ${userId}`, { error });
-  };
+  }
 }
-
-
-
-
-
