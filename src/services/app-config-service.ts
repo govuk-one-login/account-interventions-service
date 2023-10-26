c91d6879ced6832b065f46fc9dbb667e4bd333a9
Jessica <jessica.li@digital.cabinet-office.gov.uk>
2023-10-26 10:16:42 +0100

    addressing comments

--- src/services/app-config-service.ts
+++ src/services/app-config-service.ts
@@ -35,7 +35,7 @@ export class AppConfigService {
   }
 
   public get maxRetentionSeconds(): number {
-    const retentionSecond = Number.parseInt(this.validateConfiguration('DELETED_ACCOUNT_STATUS_RETENTION_SECONDS'));
+    const retentionSecond = this.validateNumberEnvVars('DELETED_ACCOUNT_STATUS_RETENTION_SECONDS');
     return retentionSecond;
   }
 
@@ -47,6 +47,17 @@ export class AppConfigService {
    * @throws {@link InvalidEnvironmentVariableError}
    * Thrown when the environment variable value is falsy
    */
+
+  private validateNumberEnvVars(environmentVariable: string): number {
+    const environmentVariableValue = Number.parseInt(this.validateConfiguration(environmentVariable));
+    if (Number.isNaN(environmentVariableValue) || environmentVariableValue < 0) {
+      const message = `${LOGS_PREFIX_INVALID_CONFIG} Environment variable ${environmentVariable} is not a number.`;
+      logger.error(message);
+      throw new InvalidEnvironmentVariableError(message);
+    }
+    return environmentVariableValue;
+  }
+
   private validateConfiguration(environmentVariable: string): string {
     if (!process.env[environmentVariable] || process.env[environmentVariable] === 'undefined') {
       const message = `${LOGS_PREFIX_INVALID_CONFIG} Environment variable ${environmentVariable} is not defined.`;
