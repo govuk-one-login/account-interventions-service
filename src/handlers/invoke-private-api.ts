2b851cbf8b206e22dc5a7df68efda75d122fcc02
Gardiner <karla.gardiner@digital.cabinet-office.gov.uk>
2023-10-26 15:24:29 +0100

    ATB-1113 updated lambda to handle userId in event

--- src/handlers/invoke-private-api.ts
+++ src/handlers/invoke-private-api.ts
@@ -1,10 +1,27 @@
 import { AppConfigService } from '../services/app-config-service';
 import logger from '../commons/logger';
 
+interface CustomEvent {
+  userId?: string;
+  headers?: {
+    [key: string]: string
+  }
+}
+
 const appConfig = AppConfigService.getInstance();
 
-export const handle = async () => {
-  const userId = appConfig.userId;
+export const handle = async (event: CustomEvent) => {
+
+  const userId = event.userId || appConfig.userId;
+  
+  if (!userId) {
+    return {
+      statusCode: 400,
+      message: "UserId is required. Provide by either adding userId to event, or providing as an environment variable."
+    }
+  }
+  
+  const headers = event.headers || { 'Content-Type': 'application/json' };
   const baseUrl = appConfig.baseUrl;
   const endpoint = appConfig.endpoint;
   const httpRequestMethod = appConfig.httpRequestMethod;
@@ -12,16 +29,25 @@ export const handle = async () => {
   const url = `${baseUrl}${endpoint}/${userId}`;
   logger.info(`invoking the url: ${url}`);
 
-  const responseJson = await fetch(url, {
+  const responsePromise = await fetch(url, {
     method: httpRequestMethod,
-    headers: {
-      'Content-Type': 'application/json',
-    },
+    headers,
   });
 
-  const response = await responseJson.json();
+  logger.debug("response json:", { responsePromise, isEmpty: Object.keys(responsePromise).length === 0 });
+
+  const response = await responsePromise.json();
+  logger.debug("parsed response:", { response });
+
+  if (Object.keys(response).length === 0 || !response.ok) {
+    return {
+      statusCode: response.status || 400,
+      message: "Request didn't return a valid response"
+    }
+  }
+
   return {
-    statusCode: 200,
+    statusCode: response.status,
     message: JSON.stringify(response),
   };
 };
