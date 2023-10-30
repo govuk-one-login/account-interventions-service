fecb49af992ef8e77f938f273b8fa1dbb94a0963
Mina Mahdi <mina.mahdi@digital.cabinet-office.gov.uk>
2023-10-30 15:31:05 +0000

    ATB-1126 Stack orchestation files

--- stack-orchestration/development_pipelines.sh
+++ stack-orchestration/development_pipelines.sh
@@ -7,5 +7,7 @@ AWS_ACCOUNT=di-id-reuse-core-dev-admin
 
 export AWS_ACCOUNT=$AWS_ACCOUNT
 
-./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.37.6 || exit 1
-./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.37.7 || exit 1
+#./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.37.6 || exit 1
+#./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.37.7 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.38.3 || exit 1
+
--- stack-orchestration/staging_pipelines.sh
+++ stack-orchestration/staging_pipelines.sh
@@ -8,7 +8,5 @@ AWS_ACCOUNT=di-id-reuse-core-staging-admin
 export AWS_ACCOUNT=$AWS_ACCOUNT
 
 ./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.38.3 || exit 1
-
 ./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.38.3 || exit 1
-
-#./provisioner.sh $AWS_ACCOUNT id-reuse-alarm-pipeline sam-deploy-pipeline v2.31.2 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.38.3 || exit 1
