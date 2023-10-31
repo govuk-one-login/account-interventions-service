95757bf8f7fb8cc95b9bf9dd5817d5685d6a2ff7
Ana-Maria Diaconu <ana-maria.diaconu@digital.cabinet-office.gov.uk>
2023-10-31 12:48:38 +0200

    removed unused file

--- src/services/test/test-validate-events.ts
+++
@@ -1,7 +0,0 @@
-import { TxMAEvent } from '../../data-types/interfaces';
-import { validateEvent, validateInterventionEvent } from '../validate-event';
-
-export const checkEvents = (event: TxMAEvent) => {
-  validateInterventionEvent(event);
-  validateEvent(event);
-};
