9ab6ea735d43c164e8209a514bad5af8f2f9e725
Bhanusri Narne <bhanu.narne@digital.cabinet-office.gov.uk>
2023-10-31 15:14:11 +0000

    added step defs

--- feature-tests/apiEndpoints/endpoints.ts
+++ feature-tests/apiEndpoints/endpoints.ts
@@ -1,15 +1,9 @@
 export default class EndPoints {
-  public static TOKEN_GENERATOR_DOMAIN = `https://mock.credential-store.${process.env.TEST_ENVIRONMENT}.account.gov.uk`;
   public static BASE_URL =
     process.env.TEST_ENVIRONMENT === 'dev'
       ? `https://${process.env.SAM_STACK_NAME}.credential-store.${process.env.TEST_ENVIRONMENT}.account.gov.uk`
       : process.env.CFN_PrivateApiEndpoint;
-  public static TOKEN_GENERATOR_PATH = '/generate';
-  public static INVALID_PATH_GENERATE_TOKEN = '/generates';
-  public static PATH_VCS = '/vcs/';
-  public static SUMMARISE_VCS = '/summarise-vcs/';
-  public static INVALID_SUMMARISE_VCS = '/sumarise-vcs/';
-  public static INVALID_PATH_VCS = '/vcs//';
+  public static INTERVENTIONS_PATH = '/ais/';
   public static BASE_SECRET_NAME =
     process.env.TEST_ENVIRONMENT === 'dev'
       ? `/${process.env.SAM_STACK_NAME}/Config/API/Key/`
--- feature-tests/tests/resources/step-definitions/delete-sns.step.ts
+++
@@ -1,5 +0,0 @@
-import { loadFeatures, autoBindSteps } from 'jest-cucumber';
-import { deleteSnsMethod } from '../shared-steps/sns-shared.steps';
-
-const features = loadFeatures('tests/resources/features/snsDelete/snsDeleteUserId-*.feature');
-autoBindSteps(features, [deleteSnsMethod]);
