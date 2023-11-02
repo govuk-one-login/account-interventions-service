3512c2aff4f3efd5c159f3560980df2e5c8e606e
Ana-Maria Diaconu <ana-maria.diaconu@digital.cabinet-office.gov.uk>
2023-11-02 16:50:11 +0200

    addressed comments

--- src/data-types/schemas.ts
+++ src/data-types/schemas.ts
@@ -2,8 +2,8 @@ export const TxMAIngress = {
   definitions: {},
   $schema: 'http://json-schema.org/draft-07/schema#',
   $id: 'https://example.com/object1689242392.json',
-  title: 'Encrypt Event Validator schema',
-  description: 'This schema validates the event that triggers Encrypt Data',
+  title: 'Fraud Risk Engine Intervention Event',
+  description: 'This schema validates the event that is received from TxMA Ingress',
   type: 'object',
   required: ['event'],
   properties: {
@@ -17,25 +17,21 @@ export const TxMAIngress = {
           $id: '#root/event/timestamp',
           title: 'Timestamp',
           type: 'number',
-          examples: [1234],
         },
         event_timestamp_ms: {
           $id: '#root/event/event_timestamp_ms',
           title: 'Event_timestamp_ms',
           type: 'number',
-          examples: [1_234_000],
         },
         event_name: {
           $id: '#root/event/event_name',
           title: 'Event_Name',
           type: 'string',
-          examples: ['TICF_ACCOUNT_INTERVENTION'],
         },
         component_id: {
           $id: '#root/event/component_id',
           title: 'Component_id',
           type: 'string',
-          examples: ['TICF_CRI'],
         },
         extension: {
           $id: '#root/event/extension',
@@ -53,13 +49,11 @@ export const TxMAIngress = {
                   $id: '#root/event/intervention_code',
                   title: 'Intervention_code',
                   type: 'string',
-                  examples: ['01', '02'],
                 },
                 intervention_reason: {
                   $id: '#root/event/intervention_reason',
                   title: 'Intervention_reason',
                   type: 'string',
-                  examples: ['reason'],
                 },
                 cms_id: {
                   $id: '#root/event/cms_id',
@@ -90,7 +84,6 @@ export const TxMAIngress = {
               $id: '#root/event/user_id',
               title: 'User_Id',
               type: 'string',
-              examples: ['urn:fdc:gov.uk:2022:JG0RJI1pYbnanbvPs-j4j5-a-PFcmhry9Qu9NCEp5d4'],
               pattern: '^[^,\\s]+$',
               minLength: 3,
             },
