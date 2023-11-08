728c816f7ba69c63fa04dfb650aabc7e2bfdb59a
Mina Mahdi <mina.mahdi@digital.cabinet-office.gov.uk>
2023-11-08 16:55:53 +0000

    re-adding checkov hooks/lambda/infra audit stacks

--- stack-orchestration/integration_bootstrap.sh
+++ stack-orchestration/integration_bootstrap.sh
@@ -7,6 +7,11 @@ AWS_ACCOUNT=di-account-interven-int-admin
 
 export AWS_ACCOUNT=$AWS_ACCOUNT
 
+
+./provisioner.sh $AWS_ACCOUNT checkov-hook checkov-hook LATEST || exit 1
+./provisioner.sh $AWS_ACCOUNT lambda-audit-hook lambda-audit-hook LATEST || exit 1
+./provisioner.sh $AWS_ACCOUNT infra-audit-hook infrastructure-audit-hook LATEST || exit 1
+
 ./provisioner.sh $AWS_ACCOUNT api-gateway-logs api-gateway-logs v1.0.4 || exit 1
 ./provisioner.sh $AWS_ACCOUNT alerting-integration alerting-integration v1.0.5 || exit 1
 ./provisioner.sh $AWS_ACCOUNT certificate-expiry certificate-expiry v1.0.4 || exit 1
