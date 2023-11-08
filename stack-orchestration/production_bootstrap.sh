819e550fef100225aa82fd46f98871a7388cdfa6
Mina Mahdi <mina.mahdi@digital.cabinet-office.gov.uk>
2023-11-08 14:24:46 +0000

    removed grafana stacks

--- stack-orchestration/production_bootstrap.sh
+++ stack-orchestration/production_bootstrap.sh
@@ -7,16 +7,7 @@ AWS_ACCOUNT=di-account-interven-prod-admin
 
 export AWS_ACCOUNT=$AWS_ACCOUNT
 
-
-./provisioner.sh $AWS_ACCOUNT checkov-hook checkov-hook LATEST || exit 1
-./provisioner.sh $AWS_ACCOUNT lambda-audit-hook lambda-audit-hook LATEST || exit 1
-./provisioner.sh $AWS_ACCOUNT infra-audit-hook infrastructure-audit-hook LATEST || exit 1
-
-./provisioner.sh $AWS_ACCOUNT grafana-key-rotation grafana-key-rotation v1.0.7 || exit 1
-./provisioner.sh $AWS_ACCOUNT grafana-metrics grafana-metrics v1.0.4 || exit 1
-
 ./provisioner.sh $AWS_ACCOUNT api-gateway-logs api-gateway-logs v1.0.4 || exit 1
-./provisioner.sh $AWS_ACCOUNT build-notifications build-notifications v2.0.1 || exit 1
 ./provisioner.sh $AWS_ACCOUNT alerting-integration alerting-integration v1.0.5 || exit 1
 ./provisioner.sh $AWS_ACCOUNT certificate-expiry certificate-expiry v1.0.4 || exit 1
 
