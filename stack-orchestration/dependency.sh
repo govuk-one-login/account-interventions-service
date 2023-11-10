1ede2bf54abdf3850a2e4f181dd267be002bee02
MinaMahdi <116737136+MinaMahdi@users.noreply.github.com>
2023-11-10 12:52:51 +0000

    ATB-1160 | Set up Integration and Production AWS Account with the dev-platform stacks (#54)
    ATB-1160

--- stack-orchestration/dependency.sh
+++ stack-orchestration/dependency.sh
@@ -28,6 +28,7 @@ function install_dependency_prompt {
 }
 
 echo "Dependency: checking whether required packages are installed"
+aws --version > /dev/null 2>&1       || install_dependency_prompt awscli
 jq --version > /dev/null 2>&1        || install_dependency_prompt jq
 gds --version > /dev/null 2>&1       || install_dependency_prompt gds-cli
 aws-vault --version > /dev/null 2>&1 || install_dependency_prompt aws-vault
---
+++ stack-orchestration/production_bootstrap.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -u
+
+# Input parameters
+AWS_ACCOUNT=di-account-interventions-production-admin
+
+export AWS_ACCOUNT=$AWS_ACCOUNT
+
+
+./provisioner.sh $AWS_ACCOUNT checkov-hook checkov-hook LATEST || exit 1
+./provisioner.sh $AWS_ACCOUNT lambda-audit-hook lambda-audit-hook LATEST || exit 1
+./provisioner.sh $AWS_ACCOUNT infra-audit-hook infrastructure-audit-hook LATEST || exit 1
+
+./provisioner.sh $AWS_ACCOUNT api-gateway-logs api-gateway-logs v1.0.4 || exit 1
+./provisioner.sh $AWS_ACCOUNT alerting-integration alerting-integration v1.0.5 || exit 1
+./provisioner.sh $AWS_ACCOUNT certificate-expiry certificate-expiry v1.0.4 || exit 1
+
+./provisioner.sh $AWS_ACCOUNT vpc vpc v2.1.0 || exit 1
