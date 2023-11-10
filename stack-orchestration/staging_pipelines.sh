e38a9614f42cc36a7a059da6818b6ea1be321481
Mina Mahdi <mina.mahdi@digital.cabinet-office.gov.uk>
2023-11-10 17:22:48 +0000

    staging bucket arn and allow accounts edited for integration

--- stack-orchestration/staging_pipelines.sh
+++ stack-orchestration/staging_pipelines.sh
@@ -7,6 +7,6 @@ AWS_ACCOUNT=di-id-reuse-core-staging-admin
 
 export AWS_ACCOUNT=$AWS_ACCOUNT
 
-./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.38.3 || exit 1
-./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.38.3 || exit 1
-./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.38.3 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.39.3 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.39.3 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.39.3 || exit 1
