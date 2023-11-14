69a1156044fa3a74ad498e09f5589db847c32073
MinaMahdi <116737136+MinaMahdi@users.noreply.github.com>
2023-11-14 12:44:14 +0000

    ATB-1161 | Establish secure pipelines for core, main, and alarm template in Integration and Production env (#61)
    * ATB-1161
    
    * staging bucket arn and allow accounts edited for integration
    
    * slack notification parameter and new parameter ArtificatSourceBucketEventTriggerRoleArn

--- stack-orchestration/production_pipelines.sh
+++ stack-orchestration/production_pipelines.sh
@@ -3,10 +3,12 @@
 set -u
 
 # Input parameters
-AWS_ACCOUNT=di-id-reuse-core-staging-admin
+AWS_ACCOUNT=di-account-interventions-production-admin
 
 export AWS_ACCOUNT=$AWS_ACCOUNT
-#
+
 ./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.39.3 || exit 1
+
 ./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.39.3 || exit 1
+
 ./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.39.3 || exit 1
