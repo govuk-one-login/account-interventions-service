c2283d5983278b3a80396455377c0338c9fea4e9
chiarapascucci-gds <chiara.pascucci@digital.cabinet-office.gov.uk>
2023-12-11 09:42:11 +0000

    returns

--- src/handlers/interventions-processor-handler.ts
+++ src/handlers/interventions-processor-handler.ts
@@ -34,6 +34,7 @@ const accountStateEngine = AccountStateEngine.getInstance();
  * It iterates over the messages polled from the queue, it processes each record, and returns the list of items that failed processing to be retried
  * @param event - SQSEvent containing the intervention event
  * @param context - context object
+ * @returns - Promise of SQS Partial Batch Response
  */
 export const handler = async (event: SQSEvent, context: Context): Promise<SQSBatchResponse> => {
   logger.addContext(context);
@@ -59,33 +60,6 @@ export const handler = async (event: SQSEvent, context: Context): Promise<SQSBat
     batchItemFailures: itemFailures,
   };
 };
-
-/**
- * Function to handle an error returned by the recording processing function
- * It logs appropriate messages and returns a message id if the Error type is not of a non-retryable type
- * @param error - error throw by the processing function
- * @param record - the record inside the message polled
- */
-async function handleError(error: unknown, record: SQSRecord) {
-  if (error instanceof ValidationError)
-    logger.warn('ValidationError caught, message will not be retried.', { errorMessage: error.message });
-  else if (error instanceof TooManyRecordsError)
-    logger.warn('Too many records were returned from the database. Message will not be retried', {
-      errorMessage: error.message,
-    });
-  else if (error instanceof StateTransitionError) {
-    logger.warn('StateTransitionError caught, message will not be retried.', { errorMessage: error.message });
-    const userId = (JSON.parse(record.body) as TxMAIngressEvent).user.user_id;
-    await sendAuditEvent('AIS_INTERVENTION_TRANSITION_IGNORED', userId, {
-      intervention: error.transition,
-      appliedAt: undefined,
-      reason: error.message,
-    });
-  } else {
-    logger.error('Error caught, message will be retried.', { errorMessage: (error as Error).message });
-    return record.messageId;
-  }
-}
 
 /**
  * Main worker function. It receives an SQS record and processes it according to business logic
@@ -138,8 +112,37 @@ async function processSQSRecord(record: SQSRecord) {
 }
 
 /**
+ * Function to handle an error returned by the recording processing function
+ * It logs appropriate messages and returns a message id if the Error type is not of a non-retryable type
+ * @param error - error throw by the processing function
+ * @param record - the record inside the message polled
+ * @returns messageId - if the message should be retried
+ */
+async function handleError(error: unknown, record: SQSRecord) {
+  if (error instanceof ValidationError)
+    logger.warn('ValidationError caught, message will not be retried.', { errorMessage: error.message });
+  else if (error instanceof TooManyRecordsError)
+    logger.warn('Too many records were returned from the database. Message will not be retried', {
+      errorMessage: error.message,
+    });
+  else if (error instanceof StateTransitionError) {
+    logger.warn('StateTransitionError caught, message will not be retried.', { errorMessage: error.message });
+    const userId = (JSON.parse(record.body) as TxMAIngressEvent).user.user_id;
+    await sendAuditEvent('AIS_INTERVENTION_TRANSITION_IGNORED', userId, {
+      intervention: error.transition,
+      appliedAt: undefined,
+      reason: error.message,
+    });
+  } else {
+    logger.error('Error caught, message will be retried.', { errorMessage: (error as Error).message });
+    return record.messageId;
+  }
+}
+
+/**
  * Helper function to obtain the intervention name based on the information inside the intervention event
  * @param recordBody - the record body from the SQS message
+ * @returns - the Enum representation of the intervention
  */
 function getInterventionName(recordBody: TxMAIngressEvent): EventsEnum {
   logger.debug('event is valid, starting processing');
@@ -173,6 +176,7 @@ async function validateAccountIsNotDeleted(intervention: EventsEnum, userId: str
 /**
  * Helper function to construct an account state object based on the data retrieved from the database
  * @param itemFromDB - query result from database
+ * @returns - Object representing the account state
  */
 function formCurrentAccountStateObject(itemFromDB?: DynamoDBStateResult) {
   return {
