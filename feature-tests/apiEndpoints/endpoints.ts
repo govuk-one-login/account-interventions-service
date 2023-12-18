d54e5c681c55ba4803a165e42f200d246d137a2f
Bhanusri Narne <bhanu.narne@digital.cabinet-office.gov.uk>
2023-12-18 15:11:02 +0000

    Update endpoints.ts file

--- feature-tests/apiEndpoints/endpoints.ts
+++ feature-tests/apiEndpoints/endpoints.ts
@@ -6,7 +6,7 @@ export default class EndPoints {
   public static SQS_QUEUE_URL =
     process.env.TEST_ENVIRONMENT === 'dev'
       ? `https://sqs.${process.env.AWS_REGION}.amazonaws.com/013758878511/${process.env.SAM_STACK_NAME}-TxMAEmulatedSourceQueue`
-      : process.env.CFN_SqsQueueUrl;
+      : process.env.CFN_TxMAEmulatedSourceQueueUrl;
   public static PATH_AIS = '/ais/';
   public static INVOKE_PRIVATE_API_GATEWAY = `${process.env.SAM_STACK_NAME}-InvokePrivateAPIGatewayFunction`;
 }
