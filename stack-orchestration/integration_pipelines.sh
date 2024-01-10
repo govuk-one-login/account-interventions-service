bf21cafd6b4e14032b2bb0ae4e65a235ad324cc4
Mina Mahdi <mina.mahdi@digital.cabinet-office.gov.uk>
2024-01-10 16:50:42 +0000

    ATB-1408

--- stack-orchestration/integration_pipelines.sh
+++ stack-orchestration/integration_pipelines.sh
@@ -7,12 +7,8 @@ AWS_ACCOUNT=di-account-interventions-integration-admin
 
 export AWS_ACCOUNT=$AWS_ACCOUNT
 
-./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.39.3 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.44.0 || exit 1
 
-./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.39.3 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.44.0 || exit 1
 
-./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.39.3 || exit 1
-
-#./provisioner.sh $AWS_ACCOUNT id-reuse-test-pipeline sam-deploy-pipeline v2.15.0 || exit 1
-#./provisioner.sh $AWS_ACCOUNT id-reuse-test-ecr test-image-repository v1.1.2 || exit 1
-
+./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.44.0 || exit 1
