b4103b8c86d7cfef9bc1ecf0fc2aa3a0439ce8bb
MinaMahdi <116737136+MinaMahdi@users.noreply.github.com>
2024-01-11 14:04:13 +0000

    ATB-1408 | Update core/main/alarm pipeline stack orchestration configuration parameters (#154)
    * ATB-1408
    
    * Remove parameterkey as not needed
    
    * PR comment spacing

--- stack-orchestration/build_pipelines.sh
+++ stack-orchestration/build_pipelines.sh
@@ -7,6 +7,6 @@ AWS_ACCOUNT=di-id-reuse-core-build-admin
 
 export AWS_ACCOUNT=$AWS_ACCOUNT
 
-./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.37.7 || exit 1
-./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.37.7 || exit 1
-./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.38.3 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.44.0 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.44.0 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.44.0 || exit 1
--- stack-orchestration/production_pipelines.sh
+++ stack-orchestration/production_pipelines.sh
@@ -7,8 +7,8 @@ AWS_ACCOUNT=di-account-interventions-production-admin
 
 export AWS_ACCOUNT=$AWS_ACCOUNT
 
-./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.39.3 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.44.0 || exit 1
 
-./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.39.3 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.44.0 || exit 1
 
-./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.39.3 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.44.0 || exit 1
