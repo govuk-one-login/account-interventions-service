234e84dedf8bbc61de22805697010fddb27ae37d
chiarapascucci-gds <chiara.pascucci@digital.cabinet-office.gov.uk>
2024-01-15 13:39:52 +0000

    delete and removed non mandatory fields

---
+++ src/contract-testing/amf-ais-consumer.pact.test.ts
@@ -0,0 +1,42 @@
+import {
+  MessageConsumerPact,
+  synchronousBodyHandler,
+  LogLevel,
+} from '@pact-foundation/pact';
+import { COMPONENT_ID } from "../data-types/constants";
+import {string} from "@pact-foundation/pact/src/v3/matchers";
+
+const path = require('path');
+const LOG_LEVEL = process.env['LOG_LEVEL'] || 'DEBUG';
+
+describe('TxMA & AIS - PACT Testing - Consumer', () => {
+  const messagePact = new MessageConsumerPact({
+    consumer: COMPONENT_ID,
+    dir: path.resolve(process.cwd(), 'pacts'),
+    pactfileWriteMode: 'update',
+    provider: 'Account Management Frontend',
+    logLevel: LOG_LEVEL as LogLevel,
+  });
+
+  describe('Incoming delete account event from Account Management Frontend', () => {
+    it('accepts a valid account deletion event', () => {
+      return (
+        messagePact
+          .given('AIS is healthy')
+          .expectsToReceive('a valid intervention event')
+          .withContent({
+              user_id: string('urn:fdc:gov.uk:2022:USER_ONE')
+          })
+          .verify(synchronousBodyHandler(syncMessageHandler))
+      )
+    });
+  });
+});
+
+function syncMessageHandler(event: any){
+  const userId = event['user_id'];
+  if( !userId || typeof userId !== 'string' || userId.trim() === ""){
+    throw new Error('Invalid Message');
+  }
+  return;
+}
--- src/contract-testing/txma-ais-consumer.pact.test.ts
+++ src/contract-testing/txma-ais-consumer.pact.test.ts
@@ -5,9 +5,9 @@ import {
   LogLevel,
 } from '@pact-foundation/pact';
 import {COMPONENT_ID} from "../data-types/constants";
-import {validateEventAgainstSchema} from "../services/validate-event";
 import {term} from "@pact-foundation/pact/src/dsl/matchers";
 import {boolean, number, string} from "@pact-foundation/pact/src/v3/matchers";
+import {validateEventAgainstSchema} from "../services/validate-event";
 const { like } = Matchers;
 
 const path = require('path');
@@ -31,18 +31,13 @@ describe('TxMA & AIS - PACT Testing - Consumer', () => {
           .withContent({
             timestamp: like(1705318190),
             event_name: 'TICF_ACCOUNT_INTERVENTION',
-            //event_id: like(12345),
-            //component_id: 'TICF_CRI',
             user: {
-              user_id: like('urn:fdc:gov.uk:2022:USER_ONE')
+              user_id: string('urn:fdc:gov.uk:2022:USER_ONE')
             },
             extensions: {
               intervention : {
                 intervention_code: term({ generate: '01', matcher: '^(01|02|03|04|05|06|07)$'}),
                 intervention_reason: string('a reason for the intervention'),
-                // originating_component_id: string('CMS'),
-                // originator_reference_id: string('123456789'),
-                // requester_id: string('987654321')
               }
             }
           })
@@ -57,8 +52,6 @@ describe('TxMA & AIS - PACT Testing - Consumer', () => {
           .withContent({
             timestamp: number(1705318190),
             event_name: like('AUTH_PASSWORD_RESET_SUCCESSFUL'),
-            //event_id: string('12345'),
-            //component_id: like('TICF_CRI'),
             user: {
               user_id: string('urn:fdc:gov.uk:2022:USER_ONE')
             }
@@ -74,8 +67,6 @@ describe('TxMA & AIS - PACT Testing - Consumer', () => {
           .withContent({
             timestamp: number(1705318190),
             event_name: like('IPV_IDENTITY_ISSUED'),
-            //event_id: string('12345'),
-            //component_id: like('TICF_CRI'),
             user: {
               user_id: string('urn:fdc:gov.uk:2022:USER_ONE')
             },
@@ -91,7 +82,6 @@ describe('TxMA & AIS - PACT Testing - Consumer', () => {
 
   });
 });
-
 
 function syncMessageHandler(event: any){
   validateEventAgainstSchema(event);
