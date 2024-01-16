1ee25f3ab7eafeb7509601b873e854f2b79b8109
Mina Mahdi <mina.mahdi@digital.cabinet-office.gov.uk>
2024-01-16 13:17:12 +0000

    ATB-1428

--- stack-orchestration/build_pipelines.sh
+++ stack-orchestration/build_pipelines.sh
@@ -10,3 +10,4 @@ export AWS_ACCOUNT=$AWS_ACCOUNT
 ./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.44.0 || exit 1
 ./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.44.0 || exit 1
 ./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.44.0 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-test-ecr test-image-repository v1.1.5 || exit 1
