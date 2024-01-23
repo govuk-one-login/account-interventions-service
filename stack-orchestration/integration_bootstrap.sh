156b922d47fbfa2e3d684ef2220548f0cdbced24
MinaMahdi <116737136+MinaMahdi@users.noreply.github.com>
2024-01-23 12:34:24 +0000

    ATB-1448 + ATB-1444 |  Update README and Update base CloudFormation stack versions (#181)
    ATB-1448 updated README and ATB-1444 updated base CF stack versions

--- stack-orchestration/integration_bootstrap.sh
+++ stack-orchestration/integration_bootstrap.sh
@@ -12,9 +12,9 @@ export AWS_ACCOUNT=$AWS_ACCOUNT
 ./provisioner.sh $AWS_ACCOUNT lambda-audit-hook lambda-audit-hook LATEST || exit 1
 ./provisioner.sh $AWS_ACCOUNT infra-audit-hook infrastructure-audit-hook LATEST || exit 1
 
-./provisioner.sh $AWS_ACCOUNT api-gateway-logs api-gateway-logs v1.0.4 || exit 1
-./provisioner.sh $AWS_ACCOUNT alerting-integration alerting-integration v1.0.5 || exit 1
-./provisioner.sh $AWS_ACCOUNT certificate-expiry certificate-expiry v1.0.4 || exit 1
+./provisioner.sh $AWS_ACCOUNT api-gateway-logs api-gateway-logs v1.0.5 || exit 1
+./provisioner.sh $AWS_ACCOUNT alerting-integration alerting-integration v1.0.6 || exit 1
+./provisioner.sh $AWS_ACCOUNT certificate-expiry certificate-expiry v1.1.0 || exit 1
 ./provisioner.sh $AWS_ACCOUNT ecr-image-scan-findings-logger ecr-image-scan-findings-logger v1.1.4 || exit 1
 
 ./provisioner.sh $AWS_ACCOUNT vpc vpc v2.3.1 || exit 1
--- stack-orchestration/production_bootstrap.sh
+++ stack-orchestration/production_bootstrap.sh
@@ -12,8 +12,8 @@ export AWS_ACCOUNT=$AWS_ACCOUNT
 ./provisioner.sh $AWS_ACCOUNT lambda-audit-hook lambda-audit-hook LATEST || exit 1
 ./provisioner.sh $AWS_ACCOUNT infra-audit-hook infrastructure-audit-hook LATEST || exit 1
 
-./provisioner.sh $AWS_ACCOUNT api-gateway-logs api-gateway-logs v1.0.4 || exit 1
-./provisioner.sh $AWS_ACCOUNT alerting-integration alerting-integration v1.0.5 || exit 1
-./provisioner.sh $AWS_ACCOUNT certificate-expiry certificate-expiry v1.0.4 || exit 1
+./provisioner.sh $AWS_ACCOUNT api-gateway-logs api-gateway-logs v1.0.5 || exit 1
+./provisioner.sh $AWS_ACCOUNT alerting-integration alerting-integration v1.0.6 || exit 1
+./provisioner.sh $AWS_ACCOUNT certificate-expiry certificate-expiry v1.1.0 || exit 1
 
 ./provisioner.sh $AWS_ACCOUNT vpc vpc v2.3.1 || exit 1
