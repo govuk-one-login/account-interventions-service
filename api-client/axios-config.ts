094cebb3fa5e636699b876548107ef302acaf59d
Liam Quayle <liam.quayle@digital.cabinet-office.gov.uk>
2024-01-26 09:26:57 +0000

    wip

--- api-client/axios-config.ts
+++ api-client/axios-config.ts
@@ -1,6 +1,10 @@
-export const axios = require('axios');
+export const axios = require('axios').default;
 
 export const instance = axios.create({
-  baseURL: 'https://072ifz6cp9.execute-api.eu-west-2.amazonaws.com/v1/ais/',
-  timeout: 1000,
+  // url: 'https://vpce-svc-0762b4928b8687636.execute-api.eu-west-2.vpce.amazonaws.com/v1',
+  method: 'get',
+  // path: '/ais/',
+  port: 80,
+  timeout: 1000
+  // host: '072ifz6cp9.execute-api.eu-west-2.amazonaws.com'
 });
--- api-client/axios-get-request.ts
+++ api-client/axios-get-request.ts
@@ -1,6 +1,15 @@
-// import { instance } from "./axiosConfig";
-import { axios } from './axios-config';
+import { instance } from "./axios-config";
+//import { axios } from './axios-config';
 
+export async function getRequest(userId: string) {
+  try {
+    await instance.get(`https://072ifz6cp9.execute-api.eu-west-2.amazonaws.com/v1/ais/${userId}`);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+//
 // export async function getRequest(user: string) {
 //   try {
 //     const response = await instance.get(user);
@@ -10,22 +19,28 @@ import { axios } from './axios-config';
 //     console.log(error);
 //   }
 // }
-export async function getRequest(userId: string) {
-  const options = {
-    baseURL: 'https://vpce-svc-0762b4928b8687636.execute-api.eu-west-2.vpce.amazonaws.com/v1',
-    Host: '072ifz6cp9.execute-api.eu-west-2.amazonaws.com',
-    path: '/ais/' + userId,
-    port: 80,
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  };
-  try {
-    await axios.get(options);
-  } catch (error) {
-    console.log(error);
-  }
-}
+      
+// 
+// const options = {
+//   url: 'https://vpce-svc-0762b4928b8687636.execute-api.eu-west-2.vpce.amazonaws.com/v1',
+//   method: 'get',
+//   Host: '072ifz6cp9.execute-api.eu-west-2.amazonaws.com',
+//   params: '/ais/' + userId,
+//   port: 80,
+//   headers: {
+//     'Content-Type': 'application/json',
+//   },
+//   timeout: 1000,
+// };
+
+//
 // headers: { Accept: 'text/html, application/json, text/plain, */*' },
 // method: 'get',
 // path: '/ais/' + userId,
+// await axios.get({
+//   url: 'https://vpce-svc-0762b4928b8687636.execute-api.eu-west-2.vpce.amazonaws.com/v1',
+//   method: 'get',
+//   path: '/ais/' + userId,
+//   port: 80,
+//   host: '072ifz6cp9.execute-api.eu-west-2.amazonaws.com',
+// });
