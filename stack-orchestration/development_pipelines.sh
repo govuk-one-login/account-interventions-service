c727bdedb23232d6d226c49f45a226ac6f32766e
Taher Kapasi <112074214+TaherKapasi@users.noreply.github.com>
2024-01-29 11:48:09 +0000

    ATB-1459: updated to use latest version of pipelines and update config for build (#187)

--- stack-orchestration/development_pipelines.sh
+++ stack-orchestration/development_pipelines.sh
@@ -4,11 +4,12 @@ set -u
 
 # Input parameters
 AWS_ACCOUNT=di-id-reuse-core-dev-admin
+PIPELINE_VERSION=v2.45.4
 
 export AWS_ACCOUNT=$AWS_ACCOUNT
 
-./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline v2.44.0 || exit 1
-./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline v2.44.0 || exit 1
-./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline v2.44.0 || exit 1
-./provisioner.sh $AWS_ACCOUNT ais-test-pipeline sam-deploy-pipeline v2.44.0 || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline $PIPELINE_VERSION || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline $PIPELINE_VERSION || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline $PIPELINE_VERSION || exit 1
+./provisioner.sh $AWS_ACCOUNT ais-test-pipeline sam-deploy-pipeline $PIPELINE_VERSION || exit 1
 ./provisioner.sh $AWS_ACCOUNT ais-test-ecr test-image-repository v1.1.5 || exit 1
