65f8cc7459a23186c886070161cdcc602a87f183
liamquayle <liam.quayle@digital.cabinet-office.gov.uk>
2024-02-01 12:24:18 +0000

    ATB 1192 | Upgrade node version from 18.* to 20.* (#197)

--- webpack.config.ts
+++ webpack.config.ts
@@ -50,7 +50,7 @@ const webpackConfig = {
     ],
   },
   resolve: { extensions: [TYPESCRIPT_FILE_EXT, JAVASCRIPT_FILE_EXT] },
-  target: ['node', 'es2022'],
+  target: ['node', 'es2023'],
   entry,
   output: {
     filename: `[name]${JAVASCRIPT_FILE_EXT}`,
