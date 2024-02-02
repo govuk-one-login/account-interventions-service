d148f151aa011d34e0e1fe531831b625da79d525
chiarapascucci-gds <chiara.pascucci@digital.cabinet-office.gov.uk>
2024-02-02 11:54:54 +0000

    madness

---
+++ esbuild.config.ts
@@ -0,0 +1,68 @@
+/* eslint-disable */
+import esbuild from 'esbuild'
+import { readFileSync } from 'fs'
+import { join } from 'path'
+import { yamlParse } from 'yaml-cfn'
+
+const SAM_TEMPLATE_DIR = './src/infra/main/template.yaml';
+
+
+interface IAwsResource {
+  Type: string
+}
+
+interface ILambdaFunction extends IAwsResource {
+  Properties: {
+    CodeUri: string
+  }
+}
+
+const lambdasPath = 'src/handlers'
+
+const { Resources } = yamlParse(readFileSync(join(__dirname, SAM_TEMPLATE_DIR), 'utf-8'));
+
+
+const awsResources = Object.values(Resources) as IAwsResource[]
+
+const lambdas = awsResources.filter(
+  (resource) => resource.Type === 'AWS::Serverless::Function'
+) as ILambdaFunction[]
+
+const entries = lambdas.map((lambda) => {
+  const lambdaNameArr = lambda.Properties.CodeUri.split('/');
+  const lambdaName = lambdaNameArr[lambdaNameArr.length-1];
+  console.log()
+  return `./${lambdasPath}/${lambdaName}.ts`
+})
+for (const entry of entries){
+  const subFolder = entry.split('/')[3]!.replaceAll('.ts', '');
+  console.log(entry);
+  esbuild.build(
+    {
+      bundle: true,
+      entryPoints: [entry],
+      logLevel: 'info',
+      minify: true,
+      platform: 'node',
+      outdir: `dist/${subFolder}`,
+      //outbase: 'src/handlers',
+      sourcesContent: false,
+      //sourcemap: true,
+      target: 'es2023'
+    }
+  ).catch(() => process.exit(1))
+}
+// esbuild
+//   .build({
+//     bundle: true,
+//     entryPoints: entries,
+//     logLevel: 'info',
+//     minify: true,
+//     platform: 'node',
+//     outdir: `dist/`,
+//     outbase: 'src/handlers',
+//     sourcesContent: false,
+//     sourcemap: true,
+//     target: 'es2022'
+//   })
+//   .catch(() => process.exit(1))
