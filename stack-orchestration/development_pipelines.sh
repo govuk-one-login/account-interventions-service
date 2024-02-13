841b0fed12ac95f31282b047a18f3f60c2983797
“Domantas <domantas.miliauskas@digital.cabinet-office.gov.uk>
2024-02-13 16:26:54 +0000

    ATB-1500 Update AIS Pipelines to v2.48.0 for Test Role SQS Permissions

--- stack-orchestration/development_pipelines.sh
+++ stack-orchestration/development_pipelines.sh
@@ -4,12 +4,11 @@ set -u
 
 # Input parameters
 AWS_ACCOUNT=di-id-reuse-core-dev-admin
-PIPELINE_VERSION=v2.45.4
+PIPELINE_VERSION=v2.48.0
 
 export AWS_ACCOUNT=$AWS_ACCOUNT
 
 ./provisioner.sh $AWS_ACCOUNT ais-core-pipeline sam-deploy-pipeline $PIPELINE_VERSION || exit 1
 ./provisioner.sh $AWS_ACCOUNT ais-main-pipeline sam-deploy-pipeline $PIPELINE_VERSION || exit 1
 ./provisioner.sh $AWS_ACCOUNT ais-alarm-pipeline sam-deploy-pipeline $PIPELINE_VERSION || exit 1
-./provisioner.sh $AWS_ACCOUNT ais-test-pipeline sam-deploy-pipeline $PIPELINE_VERSION || exit 1
 ./provisioner.sh $AWS_ACCOUNT ais-test-ecr test-image-repository v1.1.5 || exit 1
