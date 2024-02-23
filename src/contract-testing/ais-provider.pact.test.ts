fa7b692d65c1b44997f2699f2a79f6919dea9b2c
Liam Quayle <liam.quayle@digital.cabinet-office.gov.uk>
2024-02-23 11:00:14 +0000

    wip

--- src/contract-testing/ais-provider.pact.test.ts
+++ src/contract-testing/ais-provider.pact.test.ts
@@ -1,16 +1,16 @@
-import { jsonUrl } from '../../api-client/api-client-utils';
-import { VerifierOptions, Verifier } from "@pact-foundation/pact"
-import { localApiServer } from './local-api-server';
+// import { jsonUrl } from '../../api-client/api-client-utils';
+// import { VerifierOptions, Verifier } from "@pact-foundation/pact"
+// import { localApiServer } from './local-api-server';
 
-const local = new localApiServer('ais/testUserId');
-const config: VerifierOptions = {
-    providerBaseUrl: `http://127.0.0.1/3000`,
-    pactUrls: [jsonUrl],
-    provider: 'Account Intervention Service',
-    stateHandlers: {
-      'provider is healthy': async () => {
-        await local.createLocalServer();
-      },
+// const local = new localApiServer('ais/testUserId');
+// const config: VerifierOptions = {
+//     providerBaseUrl: `http://127.0.0.1/3000`,
+//     pactUrls: [jsonUrl],
+//     provider: 'Account Intervention Service',
+//     stateHandlers: {
+//       'provider is healthy': async () => {
+//         await local.createLocalServer();
+//       },
     //   'a non existing user account': async () => {
         
     //   },
@@ -32,19 +32,13 @@ const config: VerifierOptions = {
     //   'will return an unexpected error message': async () => {
   
     //   }
-    }
-  }
+//     }
+//   }
 
-  describe('PACT verification', () => {
-    beforeEach(() => {
-        jest.resetModules();
-        // process.env['TEST_ENVIRONMENT'] === 'dev';
-        // process.env['AWS_REGION'] === 'eu-west-2';
-        // process.env['SAM_STACK_NAME'] === 'ais-main';
-      });
-    it('will verify the json file', async () => {
-      const verify = new Verifier(config);
-      await verify.verifyProvider();
-    })
-  })
+//   describe('PACT verification', () => {
+//     it('will verify the json file', async () => {
+//       const verify = new Verifier(config);
+//       await verify.verifyProvider();
+//     })
+//   })
   
--- src/contract-testing/local-api-server.ts
+++ src/contract-testing/local-api-server.ts
@@ -1,24 +1,39 @@
-import * as http from 'node:http';
-import { invokeGetAccountState } from '../../feature-tests/utils/invoke-apigateway-lambda';
-// import { host } from "../../api-client/api-client-utils";
+// import * as http from 'node:http';
+// import { invokeGetAccountState } from '../../feature-tests/utils/invoke-apigateway-lambda';
+// // import { host } from "../../api-client/api-client-utils";
 
-export class localApiServer {
-  private userId: string;
-  public constructor(user: string) {
-    this.userId = user;
-  }
+// type ExpectedOutput = {
+//     statusCode: number;
+//     body: string;
+// }
+// export class localApiServer {
+//   private userId: string;
+//   public constructor(user: string) {
+//     this.userId = user;
+//   }
 
-  public async createLocalServer(): Promise<void> {
-    const server = http.createServer(async (request, res) => {
-      res.statusCode = 200;
-      res.setHeader('Content-Type', 'application/json');
-      console.log(`booting up server at: ${request.url}`);
-    });
-    server.listen(() => {
-      console.log(server.address());
-      console.log(`User ID to retrieve from API: ${this.userId}`);
-    });
-    await invokeGetAccountState(this.userId, false).catch();
-    server.closeAllConnections();
-  }
-}
+//   public async createLocalServer(): Promise<ExpectedOutput | undefined> {
+//     const port = 3000;
+//     try {
+//       const server = http.createServer(async (request, res) => {
+//         res.statusCode = 200;
+//         res.setHeader('Content-Type', 'application/json');
+//         console.log(`booting up server at: ${request.url}`);
+//       });
+//       server.listen(3000, () => {
+//         console.log(`User ID to retrieve from API: ${this.userId} with port ${port}`);
+//       });
+//       const response = await invokeGetAccountState(this.userId, false);
+//       server.closeAllConnections();
+//       return {
+//         statusCode: response.statusCode,
+//         body: response.body
+//       }
+//     } catch (error) {
+//       if (error instanceof Error) {
+//         console.error(error.name);
+//         console.error(error.message);
+//       }
+//     }
+//   }
+// }
