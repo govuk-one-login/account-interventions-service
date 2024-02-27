0ea0b4226df8d354a1fabbb81d8300264f6a656a
Bhanusri Narne <bhanu.narne@digital.cabinet-office.gov.uk>
2024-02-27 12:36:10 +0000

    added console logs

--- feature-tests/tests/resources/step-definitions/invoke-multiple-users-accounts.step.ts
+++ feature-tests/tests/resources/step-definitions/invoke-multiple-users-accounts.step.ts
@@ -49,12 +49,12 @@ defineFeature(feature, (test) => {
       for (const user of listOfUsers) {
         await timeDelayForTestEnvironment(500);
         await updateItemInTable(user, updateResetPasswordItemInTable);
-        // const getItem = await getRecordFromTable(user);
-        // if (getItem) {
-        //   console.log(getItem);
-        //   expect(getItem.resetPassword).toBe(true);
-        //   console.log("getItem", getItem.resetPassword);
-        // }
+        const getItem = await getRecordFromTable(user);
+        if (getItem) {
+          console.log(getItem);
+          expect(getItem.resetPassword).toBe(true);
+          console.log("getItem", getItem.resetPassword);
+        }
       }
     });
 
@@ -70,7 +70,7 @@ defineFeature(feature, (test) => {
         expect(response.intervention.description).toBe(interventionType);
         expect(response.state.blocked).toBe(false);
         expect(response.state.suspended).toBe(true);
-        console.log('resetPassword', response.state.resetPassword);
+        console.log("resetPassword", response.state.resetPassword);
         expect(response.state.resetPassword).toBe(true);
         expect(response.state.reproveIdentity).toBe(false);
       }
