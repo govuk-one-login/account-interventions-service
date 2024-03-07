b3c7b9de905b28fdd5dda8f9c7d3f6de72c56867
Bhanusri Narne <bhanu.narne@digital.cabinet-office.gov.uk>
2024-03-07 14:37:04 +0000

    added unHappy Path Scenarios

---
+++ feature-tests/tests/resources/step-definitions/invoke-api-gateway-unhappy-path.step.ts
@@ -0,0 +1,125 @@
+import { defineFeature, loadFeature } from 'jest-cucumber';
+import { generateRandomTestUserId } from '../../../utils/generate-random-test-user-id';
+import { sendInvalidSQSEvent, sendSQSEvent } from '../../../utils/send-sqs-message';
+import { invokeGetAccountState } from '../../../utils/invoke-apigateway-lambda';
+import { timeDelayForTestEnvironment } from '../../../utils/utility';
+import EndPoints from '../../../apiEndpoints/endpoints';
+import request from 'supertest';
+import { App } from 'supertest/types';
+
+const feature = loadFeature('./tests/resources/features/aisGET/InvokeApiGateWay-UnHappyPath.feature');
+
+defineFeature(feature, (test) => {
+  let testUserId: string;
+
+  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+  // @ts-ignore
+  let response: any;
+
+  beforeEach(() => {
+    testUserId = generateRandomTestUserId();
+  });
+
+  test('UnHappy Path - Get Request to /ais/userId - Returns Expected data for <invalidAisEventType>', ({
+    given,
+    when,
+    then,
+  }) => {
+    given(/^I send invalid (.*) intervention message to the TxMA ingress SQS queue$/, async (invalidAisEventType) => {
+      await sendInvalidSQSEvent(testUserId, invalidAisEventType);
+    });
+
+    when(/^I invoke the API to retrieve the intervention status of the account$/, async () => {
+      await timeDelayForTestEnvironment(1500);
+      response = await invokeGetAccountState(testUserId, true);
+      console.log(`Received`, { response });
+    });
+
+    then(/^I expect response with no intervention (.*)$/, async (description) => {
+      console.log(`Received`, { response });
+      expect(response.intervention.description).toBe(description);
+    });
+  });
+
+  test('UnHappy Path - Get Request to /ais/userId - Field Validation - Returns Expected Data for <aisEventType> with specific field validation', ({
+    given,
+    when,
+    then,
+  }) => {
+    given(
+      /^I send a invalid request to sqs queue with no userId and (.*), (.*) data$/,
+      async function (aisEventType, testUserId) {
+        const userId = testUserId === 'undefined' ? undefined : testUserId;
+        await sendSQSEvent(userId, aisEventType);
+      },
+    );
+
+    when(/^I invoke apiGateway to retreive the status of the invalid userId with (.*)$/, async (historyValue) => {
+      response = await invokeGetAccountState(testUserId, historyValue);
+    });
+
+    then(/^I should receive the appropriate (.*) for the ais endpoint$/, async (interventionType) => {
+      console.log(`Received`, { response });
+      expect(response.intervention.description).toBe(interventionType);
+      expect(response.auditLevel).toBe('standard');
+    });
+  });
+
+
+  test('UnHappy Path - Get Request to /ais/userId - Invalid Base URL - Returns Expected Data for <aisEventType>', ({
+    given,
+    when,
+    then,
+  }) => {
+    given(
+      /^I send a valid request to sqs queue with userId and (.*)$/,
+      async function (aisEventType) {
+        await sendSQSEvent(testUserId, aisEventType);
+      },
+    );
+
+    when(/^I invoke apiGateway with invalid base url to retreive the status of the userId$/, async () => {
+        if (process.platform === 'linux') {
+        const resultFromAPI = await request(EndPoints.AIS_BASE_URL+'/' as unknown as App)
+          .get(EndPoints.PATH_AIS + testUserId)
+          .query({ history: false })
+          .set('Content-Type', 'application/json')
+          .set('Accept', '*/*');
+         response = JSON.parse(resultFromAPI.text);
+      }
+    });
+
+    then(/^I should receive the response for the ais endpoint$/, async () => {
+      console.log(`Received`, response);
+    });
+  });
+
+
+  test('UnHappy Path - Get Request to /ais/userId - Invalid Path Parameters - Returns Expected Data for <aisEventType>', ({
+    given,
+    when,
+    then,
+  }) => {
+    given(
+      /^I send a valid request to sqs queue with (.*)$/,
+      async function (aisEventType) {
+        await sendSQSEvent(testUserId, aisEventType);
+      },
+    );
+
+    when(/^I invoke apiGateway with invalid path parameters to retreive the status of the userId$/, async () => {
+        if (process.platform === 'linux') {
+        const resultFromAPI = await request(EndPoints.AIS_BASE_URL as unknown as App)
+          .get(EndPoints.PATH_AIS+'/' + testUserId)
+          .query({ history: false })
+          .set('Content-Type', 'application/json')
+          .set('Accept', '*/*');
+         response = JSON.parse(resultFromAPI.text);
+      }
+    });
+
+    then(/^I should receive the response for the ais endpoint$/, async () => {
+      console.log(`Received`,  response );
+    });
+  });
+});
