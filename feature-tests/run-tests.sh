92bd29d0c4a1c920f56f219a2f0e344cb60697ab
Chris Bates <chris.bates@digital.cabinet-office.gov.uk>
2024-03-21 09:08:03 +0000

    update run-tests.sh tags on test run

--- feature-tests/run-tests.sh
+++ feature-tests/run-tests.sh
@@ -15,8 +15,9 @@ cp -R /utils . 2>/dev/null || :
 
 # run tests and save the exit code
 declare test_run_result
-export tagFilter=@regression
-yarn test 1>/dev/null
+export tagFilter=@test
+yarn test
+# 1>/dev/null
 test_run_result=$?
 
 # store report to dir where pipeline will export from
