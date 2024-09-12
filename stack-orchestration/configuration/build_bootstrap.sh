8f833716476f35f6164cada7b6b6f36b48618039
Taher Kapasi <taher.kapasi@digital.cabinet-office.gov.uk>
2024-09-12 13:36:31 +0100

    ATB-1819: wip

---
+++ stack-orchestration/configuration/build_bootstrap.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+set -u
+
+# Input parameters
+AWS_ACCOUNT=di-account-interventions-dev
+
+export AWS_ACCOUNT=$AWS_ACCOUNT
+
+./provisioner.sh $AWS_ACCOUNT github-identity github-identity v1.1.1 || exit 1
+./provisioner.sh $AWS_ACCOUNT aws-signer signer v1.0.8 || exit 1
+./provisioner.sh $AWS_ACCOUNT container-signer container-signer v1.1.2 || exit 1
+
+./provisioner.sh $AWS_ACCOUNT checkov-hook checkov-hook LATEST || exit 1
+./provisioner.sh $AWS_ACCOUNT lambda-audit-hook lambda-audit-hook LATEST || exit 1
+./provisioner.sh $AWS_ACCOUNT infra-audit-hook infrastructure-audit-hook LATEST || exit 1
+
+./provisioner.sh $AWS_ACCOUNT api-gateway-logs api-gateway-logs v1.0.5 || exit 1
+./provisioner.sh $AWS_ACCOUNT alerting-integration alerting-integration v1.0.6 || exit 1
+./provisioner.sh $AWS_ACCOUNT certificate-expiry certificate-expiry v1.1.1 || exit 1
+./provisioner.sh $AWS_ACCOUNT ecr-image-scan-findings-logger ecr-image-scan-findings-logger v1.2.0 || exit 1
+
+./provisioner.sh $AWS_ACCOUNT vpc vpc v2.6.0 || exit 1
