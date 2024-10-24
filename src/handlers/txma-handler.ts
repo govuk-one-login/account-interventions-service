1c11fe0a2891bb5fe08d415447b70694552dc67d
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-10-24 09:11:41 +0100

    add metrics

--- src/handlers/txma-handler.ts
+++ src/handlers/txma-handler.ts
@@ -3,6 +3,8 @@ import logger from '../commons/logger';
 import { TxMAEgressEvent } from '../data-types/interfaces';
 import { sendSqsMessage } from '../services/send-sqs-message';
 import assert from 'node:assert/strict';
+import { addMetric } from '../commons/metrics';
+import { MetricNames } from '../data-types/constants';
 
 export const handler = async (event: SQSEvent, context: Context): Promise<void> => {
   logger.addContext(context);
@@ -18,6 +20,7 @@ export const handler = async (event: SQSEvent, context: Context): Promise<void> 
   for (const record of event.Records) {
     const body: TxMAEgressEvent = JSON.parse(record.body);
     if (body.event_name === 'AUTH_DELETE_ACCOUNT') {
+      addMetric(MetricNames.RECIEVED_TXMA_ACCOUNT_DELETE);
       await sendSqsMessage(
         JSON.stringify({
           Message: record.body,
