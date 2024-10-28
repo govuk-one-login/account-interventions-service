dc56243e5eed1da98456af50407cfffd4a53b707
Saral Kaushik <saral.kaushik@digital.cabinet-office.gov.uk>
2024-10-28 10:20:01 +0000

    throw error rather than assert

--- src/handlers/txma-handler.ts
+++ src/handlers/txma-handler.ts
@@ -2,14 +2,16 @@ import { SQSEvent, Context } from 'aws-lambda';
 import logger from '../commons/logger';
 import { TxMAEgressEvent } from '../data-types/interfaces';
 import { sendSqsMessage } from '../services/send-sqs-message';
-import assert from 'node:assert/strict';
 import { addMetric, metric } from '../commons/metrics';
 import { MetricNames } from '../data-types/constants';
 
 export const handler = async (event: SQSEvent, context: Context): Promise<void> => {
   logger.addContext(context);
 
-  assert(process.env['ACCOUNT_DELETION_SQS_QUEUE'], 'ACCOUNT_DELETION_SQS_QUEUE env variable is not set');
+  if (!process.env['ACCOUNT_DELETION_SQS_QUEUE']) {
+    logger.error('ACCOUNT_DELETION_SQS_QUEUE env variable is not set');
+    throw new Error('ACCOUNT_DELETION_SQS_QUEUE env variable is not set');
+  }
   const accountDeletionSqsQueue = process.env['ACCOUNT_DELETION_SQS_QUEUE'];
 
   if (!event.Records[0]) {
--- src/services/send-sqs-message.ts
+++ src/services/send-sqs-message.ts
@@ -1,11 +1,14 @@
 import { SQSClient, SendMessageRequest, SendMessageCommand, SendMessageCommandOutput } from '@aws-sdk/client-sqs';
-import assert from 'node:assert/strict';
+import logger from '../commons/logger';
 
 export const sendSqsMessage = async (
   messageBody: string,
   queueUrl: string | undefined,
 ): Promise<SendMessageCommandOutput> => {
-  assert(process.env['AWS_REGION'], 'AWS_REGION environment variable not set');
+  if (!process.env['AWS_REGION']) {
+    logger.error('AWS_REGION environment variable is not set');
+    throw new Error('AWS_REGION environment variable not set');
+  }
   const region = process.env['AWS_REGION'];
   const client = new SQSClient({ region });
   const message: SendMessageRequest = {
