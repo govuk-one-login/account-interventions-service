a847a419d7952f24295842dd009715e60c3520d3
Yasser Khan <yasser.khan@digital.cabinet-office.gov.uk>
2026-02-09 09:49:01 +0000

    Removed checkov stack

--- stack-orchestration/configuration/build_bootstrap.sh
+++ stack-orchestration/configuration/build_bootstrap.sh
@@ -11,7 +11,6 @@ export AWS_ACCOUNT=$AWS_ACCOUNT
 ./provisioner.sh $AWS_ACCOUNT aws-signer signer v1.0.8 || exit 1
 ./provisioner.sh $AWS_ACCOUNT container-signer container-signer v1.1.2 || exit 1
 
-./provisioner.sh $AWS_ACCOUNT checkov-hook checkov-hook LATEST || exit 1
 ./provisioner.sh $AWS_ACCOUNT lambda-audit-hook lambda-audit-hook LATEST || exit 1
 ./provisioner.sh $AWS_ACCOUNT infra-audit-hook infrastructure-audit-hook LATEST || exit 1
 
