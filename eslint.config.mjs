cc6ec2728d771230dfb1269a53e3c90f24ef0342
Edward Louth <edward.louth@digital.cabinet-office.gov.uk>
2026-03-16 11:08:59 +0000

    FPAD-5620: Format eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import eslintPluginUnicorn from 'eslint-plugin-unicorn';
-import eslintPluginTsdoc from 'eslint-plugin-tsdoc'
+import eslintPluginTsdoc from 'eslint-plugin-tsdoc';
 import globals from 'globals';
 
 export default defineConfig(
@@ -15,31 +15,31 @@ export default defineConfig(
     files: ['**/*.ts'],
     extends: [eslint.configs.recommended, tseslint.configs.strictTypeChecked],
     languageOptions: {
-            globals: globals.builtin,
+      globals: globals.builtin,
       parserOptions: {
         projectService: true,
       },
-        },
+    },
     plugins: {
-            unicorn: eslintPluginUnicorn,
-      tsdoc: eslintPluginTsdoc
-        },
+      unicorn: eslintPluginUnicorn,
+      tsdoc: eslintPluginTsdoc,
+    },
     rules: {
-    '@typescript-eslint/no-floating-promises': ['error'],
-    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
-    '@typescript-eslint/no-explicit-any': ['warn'],
-    'keyword-spacing': ['error', { after: true }],
-    'space-before-blocks': ['error', 'always'],
-    'space-before-function-paren': ['error', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
-    'object-curly-spacing': ['error', 'always'],
-    'unicorn/filename-case': ['error', { case: 'kebabCase' }],
-    'unicorn/prevent-abbreviations': ['warn'],
-    'unicorn/prefer-string-raw': ['off'],
-    'unicorn/error-message': ['warn'],
-    'unicorn/no-array-reduce': ['off'],
-    'unicorn/prefer-logical-operator-over-ternary': ['warn'],
-    'unicorn/prefer-spread': ['warn'],
-    'tsdoc/syntax': ['warn'],
+      '@typescript-eslint/no-floating-promises': ['error'],
+      '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': ['warn'],
+      'keyword-spacing': ['error', { after: true }],
+      'space-before-blocks': ['error', 'always'],
+      'space-before-function-paren': ['error', { anonymous: 'always', named: 'never', asyncArrow: 'always' }],
+      'object-curly-spacing': ['error', 'always'],
+      'unicorn/filename-case': ['error', { case: 'kebabCase' }],
+      'unicorn/prevent-abbreviations': ['warn'],
+      'unicorn/prefer-string-raw': ['off'],
+      'unicorn/error-message': ['warn'],
+      'unicorn/no-array-reduce': ['off'],
+      'unicorn/prefer-logical-operator-over-ternary': ['warn'],
+      'unicorn/prefer-spread': ['warn'],
+      'tsdoc/syntax': ['warn'],
+    },
   },
-  }
 );
