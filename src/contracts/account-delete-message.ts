aa85e25bc439bf184f8a5ec6d563ddba0a9b0784
Edward Louth <edward.louth@digital.cabinet-office.gov.uk>
2026-04-28 16:05:29 +0100

    FPAD-7910: Add handling for AUTH_DELETE_ACCOUNT to Account Deletion Processor
    The wrapped user_id and name matches the AUTH_DELETE_ACCOUNT event.  As zod is permissive this will pass for the full event.

--- src/contracts/account-delete-message.ts
+++ src/contracts/account-delete-message.ts
@@ -1,8 +1,14 @@
 import { z } from 'zod';
 
-export const accountDeleteMessageSchema = z.object({
-  event_name: z.literal('AUTH_DELETE_ACCOUNT'),
-  user_id: z.string().optional(),
-});
+export const accountDeleteMessageSchema = z.union([
+  z.object({
+    event_name: z.literal('AUTH_DELETE_ACCOUNT'),
+    user: z.object({ user_id: z.string().optional() }),
+  }),
+  z.object({
+    event_name: z.literal('AUTH_DELETE_ACCOUNT'),
+    user_id: z.string().optional(),
+  }),
+]);
 
 export type AccountDeleteMessage = z.infer<typeof accountDeleteMessageSchema>;
