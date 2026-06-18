66b9f5f947c588a183934f8b7172266961a8b9d3
Edward Louth <edward.louth@digital.cabinet-office.gov.uk>
2026-06-18 17:40:32 +0100

    FPAD-8283: Add account-status table

---
+++ src/tables/account-status.ts
@@ -0,0 +1,76 @@
+import z from 'zod';
+import { getDBDocumentClient } from '../services/database-client';
+import { DynamoDBRecordService, RecordService } from '../services/dynamo-db-record-service';
+import TableConfig from './table-config';
+import { AppConfigService } from '../services/app-config-service';
+
+const appConfig = AppConfigService.getInstance();
+
+const schema = z.object({
+  sentAt: z.number(),
+  appliedAt: z.number(),
+  isAccountDeleted: z.boolean(),
+  history: z.array(z.string()),
+  intervention: z.string(),
+  blocked: z.boolean(),
+  suspended: z.boolean(),
+  resetPassword: z.boolean(),
+  reproveIdentity: z.boolean(),
+});
+
+export const accountStatusTableConfig: TableConfig<typeof schema> = {
+  tableName: appConfig.tableName,
+  partitionKeyName: 'pk',
+  schema,
+};
+
+export type AccountStatus = z.infer<typeof schema>;
+
+export interface AccountStatusService {
+  getAccountStateInformation(accountId: string): Promise<AccountStatus | undefined>;
+  getFullAccountInformation(accountId: string): Promise<AccountStatus | undefined>;
+}
+
+export class InMemoryAccountStatusService implements AccountStatusService {
+  constructor(
+    readonly status?: AccountStatus,
+    readonly error?: Error,
+  ) {}
+
+  getAccountStateInformation() {
+    if (this.error) return Promise.reject(this.error);
+
+    return Promise.resolve(this.status);
+  }
+
+  getFullAccountInformation() {
+    if (this.error) return Promise.reject(this.error);
+
+    return Promise.resolve(this.status);
+  }
+}
+
+export class PersistentAccountStatusService implements AccountStatusService {
+  constructor(private readonly recordService: RecordService<typeof schema>) {}
+
+  getAccountStateInformation(accountId: string) {
+    return this.recordService.getByPkAndValidate(accountId, [
+      'blocked',
+      'suspended',
+      'resetPassword',
+      'reproveIdentity',
+      'sentAt',
+      'appliedAt',
+      'isAccountDeleted',
+      'history',
+      'intervention',
+    ]);
+  }
+
+  getFullAccountInformation(accountId: string) {
+    return this.recordService.getByPkAndValidate(accountId);
+  }
+}
+
+export const getPersistentAccountStatusService = (): PersistentAccountStatusService =>
+  new PersistentAccountStatusService(new DynamoDBRecordService(accountStatusTableConfig, getDBDocumentClient()));
