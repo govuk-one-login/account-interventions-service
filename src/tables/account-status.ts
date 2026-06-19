3db41e7b4bccd5cb9f88aaf3047736e5558c6b59
Edward Louth <edward.louth@digital.cabinet-office.gov.uk>
2026-06-19 11:37:23 +0100

    FPAD-8283: Move interventions-processor-handler to account-status typed service

--- src/tables/account-status.ts
+++ src/tables/account-status.ts
@@ -5,6 +5,11 @@ import TableConfig from './table-config';
 import { AppConfigService } from '../services/app-config-service';
 import { getCurrentTimestamp } from '../commons/get-current-timestamp';
 import { UpdateCommandOutput } from '@aws-sdk/lib-dynamodb';
+import { AccountStateEngineOutput, CurrentTimeDescriptor } from '../data-types/interfaces';
+import { InterventionEventMessage } from '../contracts/intervention-events';
+import { LOGS_PREFIX_SENSITIVE_INFO } from '../data-types/constants';
+import { buildPartialUpdateAccountStateCommand } from '../commons/build-partial-update-state-command';
+import logger from '../commons/logger';
 
 const appConfig = AppConfigService.getInstance();
 
@@ -31,6 +36,13 @@ export type AccountStatus = z.infer<typeof schema>;
 export interface AccountStatusService {
   getAccountStateInformation(accountId: string): Promise<AccountStatus | undefined>;
   getFullAccountInformation(accountId: string): Promise<AccountStatus | undefined>;
+  updateUserStatus(
+    accountId: string,
+    statusResult: AccountStateEngineOutput,
+    currentTimestamp: CurrentTimeDescriptor,
+    result: InterventionEventMessage,
+    historyList: string[],
+  ): Promise<void>;
   updateDeleteStatus(accountId: string): Promise<UpdateCommandOutput | undefined>;
 }
 
@@ -50,6 +62,10 @@ export class InMemoryAccountStatusService implements AccountStatusService {
     if (this.error) return Promise.reject(this.error);
 
     return Promise.resolve(this.status);
+  }
+
+  updateUserStatus() {
+    return Promise.resolve(undefined);
   }
 
   updateDeleteStatus() {
@@ -78,6 +94,24 @@ export class PersistentAccountStatusService implements AccountStatusService {
 
   getFullAccountInformation(accountId: string) {
     return this.recordService.getByPkAndValidate(accountId);
+  }
+
+  async updateUserStatus(
+    accountId: string,
+    statusResult: AccountStateEngineOutput,
+    currentTimestamp: CurrentTimeDescriptor,
+    result: InterventionEventMessage,
+    historyList: string[],
+  ) {
+    const partialCommandInput = buildPartialUpdateAccountStateCommand(
+      statusResult.stateResult,
+      currentTimestamp.milliseconds,
+      result,
+      historyList,
+      statusResult.interventionName,
+    );
+    logger.debug(`${LOGS_PREFIX_SENSITIVE_INFO} Updating user status`, { userId: accountId, partialCommandInput });
+    await this.recordService.update(accountId, partialCommandInput);
   }
 
   updateDeleteStatus(accountId: string) {
