5bcaf8e02a7676d9d2ce44969bfda61201e7e0eb
Edward Louth <edward.louth@digital.cabinet-office.gov.uk>
2026-07-15 15:18:54 +0100

    FPAD-8468: Allow FAI to access AIS frontend
    Handle STAGE_PREFIX and SUBPATH for different Public API Gateway setups

--- src/handlers/frontend-handler.ts
+++ src/handlers/frontend-handler.ts
@@ -4,6 +4,15 @@ import awsLambdaFastify from '@fastify/aws-lambda';
 import { APIGatewayProxyEvent, Context } from 'aws-lambda';
 import { init } from '../frontend/app';
 
+const subpath = process.env['SUBPATH'] ?? '';
+
 const proxy = awsLambdaFastify(init());
 
-export const handler = (event: APIGatewayProxyEvent, context: Context) => proxy(event, context);
+export const handler = (event: APIGatewayProxyEvent, context: Context) => proxy(rewriteEventPath(event), context);
+
+const rewriteEventPath = (event: APIGatewayProxyEvent): APIGatewayProxyEvent => {
+  if (subpath && event.path.startsWith(subpath)) {
+    event.path = event.path.slice(subpath.length) || '/';
+  }
+  return event;
+};
