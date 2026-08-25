20748a05e85759f06c9b4b7b1b6af7cb50b30232
Kirsty Allen <kirsty.allen@digital.cabinet-office.gov.uk>
2026-08-25 18:34:28 +0100

    FPAD-8564: Fix unit tests and write new unit tests for updated implementation

--- src/frontend/tests/app.snapshot.test.ts
+++ src/frontend/tests/app.snapshot.test.ts
@@ -3,6 +3,7 @@ import { InterventionStub, InterventionName, InterventionState } from '@govuk-on
 import { StubAuthoriser } from '../authoriser';
 import { StubMessageService } from '../../services/message-service';
 import { FeatureFlagsStub } from '../../services/feature-flags';
+import { RedirectChecker } from '../redirect-handler';
 
 vi.mock('@aws-lambda-powertools/logger');
 
@@ -18,6 +19,7 @@ describe('rendered page snapshots', () => {
         interventionClient: new InterventionStub({ result: { interventions: [] } }),
         messageService: new StubMessageService(),
         authoriser: new StubAuthoriser(),
+        redirectHandler: new RedirectChecker(),
       },
       {
         featureFlags: new FeatureFlagsStub({ aisFrontend: true, aisSendTxMA: true }),
@@ -56,6 +58,7 @@ describe('rendered page snapshots', () => {
         }),
         messageService: new StubMessageService(),
         authoriser: new StubAuthoriser(),
+        redirectHandler: new RedirectChecker(),
       },
       {
         featureFlags: new FeatureFlagsStub({ aisFrontend: true, aisSendTxMA: true }),
@@ -158,6 +161,7 @@ describe('rendered page snapshots', () => {
         interventionClient: new InterventionStub({ result: { interventions: [] } }),
         messageService: new StubMessageService(),
         authoriser: new StubAuthoriser(),
+        redirectHandler: new RedirectChecker(),
       },
       {
         featureFlags: new FeatureFlagsStub({ aisFrontend: true, aisSendTxMA: true }),
