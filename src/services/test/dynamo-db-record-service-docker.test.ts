e1ca0d4afda7523322fc3e53eb428fc06a5664bf
jason cowlam <jason.cowlam@digital.cabinet-office.gov.uk>
2026-09-21 16:16:39 +0100

    FPAD-8439: Add docker tests for TTL backfill service

--- src/services/test/dynamo-db-record-service-docker.test.ts
+++ src/services/test/dynamo-db-record-service-docker.test.ts
@@ -1,56 +1,20 @@
 import z from 'zod';
 import TableConfig from '../../tables/table-config';
 import { DynamoDBRecordService } from '../dynamo-db-record-service';
-import { DynamoDBClient, CreateTableCommand } from '@aws-sdk/client-dynamodb';
+import { CreateTableCommand, DeleteTableCommand, DynamoDBClient } from '@aws-sdk/client-dynamodb';
 import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
 import 'aws-sdk-client-mock-vitest/extend';
 
-
-// test just for running docker tests
-// const ddbMock = mockClient(DynamoDBDocumentClient);
-
-
 const isDynamoDbLocal = () => process.env['TEST_DYNAMODB_LOCAL'] === 'true';
 
-const createLocalClient = async () => {
 
-  //ddbMock.restore();
-
-  const localClient = DynamoDBDocumentClient.from(
+const createLocalClient = () => DynamoDBDocumentClient.from(
     new DynamoDBClient({
       endpoint: 'http://localhost:8000',
       region: 'eu-west-2',
       credentials: { accessKeyId: 'test', secretAccessKey: 'test' },
     }),
-  );
-
-  try {
-    // creates table todo move to recreate every time
-    await localClient.send(
-      new CreateTableCommand({
-        TableName: 'test-table',
-        KeySchema: [{ AttributeName: 'pk1', KeyType: 'HASH' }],
-        AttributeDefinitions: [{ AttributeName: 'pk1', AttributeType: 'S' }],
-        BillingMode: 'PAY_PER_REQUEST',
-      }),
-    );
-    console.log('Table created');
-  } catch (error) {
-    console.log('Table creation error:', error);
-  }
-
-  return localClient;
-}
-
-
-const localClient = isDynamoDbLocal() && (await createLocalClient());
-
-function getTestClient() {
-  if (isDynamoDbLocal()) {
-    return localClient;
-  }
- // return ddbMock;
-}
+  )
 
 const schema = z.object({
   pk1: z.string(),
@@ -64,259 +28,94 @@ const tableConfig: TableConfig<typeof schema> = {
   schema,
 };
 
-beforeEach(() => {
-  // if (!isDynamoDbLocal()) {
-  //   ddbMock.reset();
-  // }
-});
+describe.skipIf(!isDynamoDbLocal())('DynamoDBRecordService (Docker)', { tags: ['dynamodb-local'] }, () => {
 
-describe.skipIf(!isDynamoDbLocal())('DynamoDBRecordService', { tags: ['dynamodb-local'] }, () => {
-  test('queryByPkAndValidate', async () => {
-    const client = getTestClient();
-    // ddbMock.on(QueryCommand).resolves({
-    //   Items: [
-    //     {
-    //       pk1: 'value1',
-    //     },
-    //   ],
-    // });
+  let localClient: DynamoDBClient;
+  beforeAll(() => {
+    localClient = createLocalClient();
+  })
 
-    const service = new DynamoDBRecordService<typeof schema>(tableConfig, client as unknown as DynamoDBDocumentClient);
-
-    if (isDynamoDbLocal()) {
-      await service.batchWrite([
-        {
-          pk1: 'value1',
-        },
-      ]);
+  beforeEach(async () => {
+    try {
+      await localClient.send(
+        new DeleteTableCommand({
+          TableName: 'test-table',
+        }),
+      );
+    } catch {
+      // Table may not exist on first run
     }
 
-    const response = await service.queryByPkAndValidate('value1');
+    await localClient.send(
+      new CreateTableCommand({
+        TableName: 'test-table',
+        KeySchema: [{ AttributeName: 'pk1', KeyType: 'HASH' }],
+        AttributeDefinitions: [{ AttributeName: 'pk1', AttributeType: 'S' }],
+        BillingMode: 'PAY_PER_REQUEST',
+      }),
+    );
+  })
 
-    // both @dynamodb-local and ddbMock return the same result
-    expect(response).toEqual([
+  test('queryByPkAndValidate', async () => {
+    const service = new DynamoDBRecordService<typeof schema>(tableConfig, localClient);
+    await service.batchWrite([
       {
         pk1: 'value1',
       },
     ]);
 
-    // if (!isDynamoDbLocal()) {
-    //   expect(ddbMock).toHaveReceivedCommandWith(QueryCommand, {
-    //     TableName: 'test-table',
-    //     KeyConditionExpression: '#pk = :pk',
-    //     ExpressionAttributeNames: { '#pk': 'pk1' },
-    //     ExpressionAttributeValues: { ':pk': 'value1' },
-    //   });
-    // }
+    const response = await service.queryByPkAndValidate('value1');
+
+    expect(response).toEqual([
+      {
+        pk1: 'value1',
+      },
+    ]);
   });
 
-  // test('queryByPkAndValidate includedKeys', async () => {
-  //   ddbMock.on(QueryCommand).resolves({
-  //     Items: [
-  //       {
-  //         pk1: 'value1',
-  //       },
-  //     ],
-  //   });
-  //
-  //   const service = new DynamoDBRecordService<typeof schema>(tableConfig, ddbMock as unknown as DynamoDBDocumentClient);
-  //
-  //   const res = await service.queryByPkAndValidate('key_value_1', ['pk1']);
-  //
-  //   expect(res).toEqual([
-  //     {
-  //       pk1: 'value1',
-  //     },
-  //   ]);
-  //
-  //   expect(ddbMock).toHaveReceivedCommandWith(QueryCommand, {
-  //     TableName: 'test-table',
-  //     KeyConditionExpression: '#pk = :pk',
-  //     ExpressionAttributeNames: { '#pk': 'pk1' },
-  //     ExpressionAttributeValues: { ':pk': 'key_value_1' },
-  //   });
-  // });
+  test('queryByPkAndValidate includedKeys when no matching items', async () => {
+    const service = new DynamoDBRecordService<typeof schema>(tableConfig, localClient);
 
-  // test('queryByPkAndValidate includedKeys not including otherwise required field', async () => {
-  //   const schema2 = schema.safeExtend({
-  //     requiredKey: z.string(),
-  //   });
-  //
-  //   ddbMock.on(QueryCommand).resolves({
-  //     Items: [
-  //       {
-  //         pk1: 'value1',
-  //       },
-  //     ],
-  //   });
-  //
-  //   const service = new DynamoDBRecordService<typeof schema2>(
-  //     { ...tableConfig, schema: schema2 },
-  //     ddbMock as unknown as DynamoDBDocumentClient,
-  //   );
-  //
-  //   const res = await service.queryByPkAndValidate('key_value_1', ['pk1']);
-  //
-  //   expect(res).toEqual([
-  //     {
-  //       pk1: 'value1',
-  //     },
-  //   ]);
-  //
-  //   expect(ddbMock).toHaveReceivedCommandWith(QueryCommand, {
-  //     TableName: 'test-table',
-  //     KeyConditionExpression: '#pk = :pk',
-  //     ExpressionAttributeNames: { '#pk': 'pk1' },
-  //     ExpressionAttributeValues: { ':pk': 'key_value_1' },
-  //   });
-  // });
-  //
-  // test('queryByPkAndValidate includedKeys missing required field', async () => {
-  //   const schema2 = schema.safeExtend({
-  //     requiredKey: z.string(),
-  //   });
-  //
-  //   ddbMock.on(QueryCommand).resolves({
-  //     Items: [
-  //       {
-  //         pk1: 'value1',
-  //       },
-  //     ],
-  //   });
-  //
-  //   const service = new DynamoDBRecordService<typeof schema2>(
-  //     { ...tableConfig, schema: schema2 },
-  //     ddbMock as unknown as DynamoDBDocumentClient,
-  //   );
-  //
-  //   await expect(service.queryByPkAndValidate('key_value_1', ['pk1', 'requiredKey'])).rejects.toThrow(ZodError);
-  //
-  //   expect(ddbMock).toHaveReceivedCommandWith(QueryCommand, {
-  //     TableName: 'test-table',
-  //     KeyConditionExpression: '#pk = :pk',
-  //     ExpressionAttributeNames: { '#pk': 'pk1' },
-  //     ExpressionAttributeValues: { ':pk': 'key_value_1' },
-  //   });
-  // });
-  //
-  // test('queryByPkAndValidate empty', async () => {
-  //   ddbMock.on(QueryCommand).resolves({
-  //     Items: [],
-  //   });
-  //
-  //   const service = new DynamoDBRecordService<typeof schema>(tableConfig, ddbMock as unknown as DynamoDBDocumentClient);
-  //
-  //   const response = await service.queryByPkAndValidate('key_value_1');
-  //
-  //   expect(response).toEqual([]);
-  //
-  //   expect(ddbMock).toHaveReceivedCommandWith(QueryCommand, {
-  //     TableName: 'test-table',
-  //     KeyConditionExpression: '#pk = :pk',
-  //     ExpressionAttributeNames: { '#pk': 'pk1' },
-  //     ExpressionAttributeValues: { ':pk': 'key_value_1' },
-  //   });
-  // });
-  //
-  // test('queryByPkAndValidate no Items', async () => {
-  //   ddbMock.on(QueryCommand).resolves({
-  //     Items: undefined,
-  //   });
-  //
-  //   const service = new DynamoDBRecordService<typeof schema>(tableConfig, ddbMock as unknown as DynamoDBDocumentClient);
-  //
-  //   const response = await service.queryByPkAndValidate('key_value_1');
-  //
-  //   expect(response).toEqual([]);
-  //
-  //   expect(ddbMock).toHaveReceivedCommandWith(QueryCommand, {
-  //     TableName: 'test-table',
-  //     KeyConditionExpression: '#pk = :pk',
-  //     ExpressionAttributeNames: { '#pk': 'pk1' },
-  //     ExpressionAttributeValues: { ':pk': 'key_value_1' },
-  //   });
-  // });
-  //
-  // test('getByPkAndValidate', async () => {
-  //   ddbMock.on(QueryCommand).resolves({
-  //     Items: [
-  //       {
-  //         pk1: 'value1',
-  //       },
-  //     ],
-  //   });
-  //
-  //   const service = new DynamoDBRecordService<typeof schema>(tableConfig, ddbMock as unknown as DynamoDBDocumentClient);
-  //
-  //   const res = await service.getByPkAndValidate('key_value_1');
-  //
-  //   expect(res).toEqual({
-  //     pk1: 'value1',
-  //   });
-  //
-  //   expect(ddbMock).toHaveReceivedCommandWith(QueryCommand, {
-  //     TableName: 'test-table',
-  //     KeyConditionExpression: '#pk = :pk',
-  //     ExpressionAttributeNames: { '#pk': 'pk1' },
-  //     ExpressionAttributeValues: { ':pk': 'key_value_1' },
-  //   });
-  // });
-  //
-  // test('getByPkAndValidate includedKeys', async () => {
-  //   ddbMock.on(QueryCommand).resolves({
-  //     Items: [
-  //       {
-  //         pk1: 'value1',
-  //         isAccountDeleted: true,
-  //         abc: 1234,
-  //       },
-  //     ],
-  //   });
-  //
-  //   const service = new DynamoDBRecordService<typeof schema>(tableConfig, ddbMock as unknown as DynamoDBDocumentClient);
-  //
-  //   const res = await service.getByPkAndValidate('key_value_1', ['pk1', 'isAccountDeleted']);
-  //
-  //   expect(res).toEqual({
-  //     pk1: 'value1',
-  //     isAccountDeleted: true,
-  //   });
-  //
-  //   expect(ddbMock).toHaveReceivedCommandWith(QueryCommand, {
-  //     TableName: 'test-table',
-  //     KeyConditionExpression: '#pk = :pk',
-  //     ExpressionAttributeNames: { '#pk': 'pk1' },
-  //     ExpressionAttributeValues: { ':pk': 'key_value_1' },
-  //     ProjectionExpression: 'pk1,isAccountDeleted',
-  //   });
-  // });
-  //
-  // test('getByPkAndValidate empty', async () => {
-  //   ddbMock.on(QueryCommand).resolves({
-  //     Items: [],
-  //   });
-  //
-  //   const service = new DynamoDBRecordService<typeof schema>(tableConfig, ddbMock as unknown as DynamoDBDocumentClient);
-  //
-  //   const res = await service.getByPkAndValidate('key_value_1');
-  //
-  //   expect(res).toEqual(undefined);
-  //
-  //   expect(ddbMock).toHaveReceivedCommandWith(QueryCommand, {
-  //     TableName: 'test-table',
-  //     KeyConditionExpression: '#pk = :pk',
-  //     ExpressionAttributeNames: { '#pk': 'pk1' },
-  //     ExpressionAttributeValues: { ':pk': 'key_value_1' },
-  //   });
-  // });
+    await service.batchWrite([
+      {
+        pk1: 'value1',
+      },
+    ]);
+
+    const response = await service.queryByPkAndValidate('value1', ['isAccountDeleted']);
+
+    expect(response).toEqual([{}]);
+  });
+
+  test('queryByPkAndValidate includedKeys with matching items', async () => {
+    const service = new DynamoDBRecordService<typeof schema>(tableConfig, localClient);
+
+    await service.batchWrite([
+      {
+        pk1: 'value1',
+        isAccountDeleted: false,
+        resetPasswordAt: '1000',
+      },
+    ]);
+
+    const response = await service.queryByPkAndValidate('value1', ['isAccountDeleted']);
+
+    expect(response).toEqual([
+      {
+        isAccountDeleted: false
+      },
+    ]);
+  });
+
+  test('queryByPkAndValidate when no matching pk', async () => {
+    const service = new DynamoDBRecordService<typeof schema>(tableConfig, localClient);
+    const response = await service.queryByPkAndValidate('value1', ['isAccountDeleted']);
+
+    expect(response).toEqual([]);
+  });
 
   test('batchWrite', async () => {
-    const client = getTestClient();
-    const service = new DynamoDBRecordService<typeof schema>(tableConfig, client as unknown as DynamoDBDocumentClient);
-
-    // if (!isDynamoDbLocal()) {
-    //   ddbMock.on(BatchWriteCommand).resolves({});
-    // }
+    const service = new DynamoDBRecordService<typeof schema>(tableConfig, localClient);
 
     const res = await service.batchWrite([
       {
@@ -324,86 +123,44 @@ describe.skipIf(!isDynamoDbLocal())('DynamoDBRecordService', { tags: ['dynamodb-
       },
     ]);
 
-    // if (!isDynamoDbLocal()) {
-    //   expect(res).toEqual({});
-    //   expect(ddbMock).toHaveReceivedCommandWith(BatchWriteCommand, {
-    //     RequestItems: {
-    //       'test-table': [
-    //         {
-    //           PutRequest: {
-    //             Item: { pk1: 'value1' },
-    //           },
-    //         },
-    //       ],
-    //     },
-    //   });
-    // }
-
     // test the return result from the dynamodb
-    if (isDynamoDbLocal()) {
-      expect(res.$metadata.httpStatusCode).toBe(200);
-      const results = await service.queryByPkAndValidate('value1');
-      expect(results).toEqual([{ pk1: 'value1' }]);
-    }
+    expect(res.$metadata.httpStatusCode).toBe(200);
+    const results = await service.queryByPkAndValidate('value1');
+    expect(results).toEqual([{ pk1: 'value1' }]);
   });
 
   test('basic update', async () => {
-    const client = getTestClient();
-
-    const service = new DynamoDBRecordService<typeof schema>(tableConfig, client as unknown as DynamoDBDocumentClient);
+    const service = new DynamoDBRecordService<typeof schema>(tableConfig, localClient);
 
     // we need to add the item in
     // consider adding this via the getTestClient as we create the table?
-    if (isDynamoDbLocal()) {
-      await service.batchWrite([
-        {
-          pk1: '1234',
-        },
-      ]);
-    }
+    await service.batchWrite([
+      {
+        pk1: '1234',
+      },
+    ]);
+
     await service.update('1234', {
       isAccountDeleted: true,
     });
 
-    // if (!isDynamoDbLocal()) {
-    //   expect(ddbMock).toHaveReceivedCommandWith(UpdateCommand, {
-    //     TableName: 'test-table',
-    //     Key: {
-    //       pk1: '1234',
-    //     },
-    //     UpdateExpression: 'SET #isAccountDeleted = :isAccountDeleted',
-    //     ExpressionAttributeNames: {
-    //       '#isAccountDeleted': 'isAccountDeleted',
-    //     },
-    //     ExpressionAttributeValues: {
-    //       ':isAccountDeleted': true,
-    //     },
-    //   });
-    // }
-
-    if (isDynamoDbLocal()) {
-      const results = await service.queryByPkAndValidate('1234');
-      expect(results).toEqual([{ pk1: '1234', isAccountDeleted: true }]);
-    }
+    const results = await service.queryByPkAndValidate('1234');
+    expect(results).toEqual([{ pk1: '1234', isAccountDeleted: true }]);
   });
 
   // note the attempt to delete the primary key is an invalid operation in DynamoDB
   // you need to delete the entire item
   // running this test through the dynamodb-local docker image throws an error
   // added an extra key to the schema resetPasswordAt to remove instead of the pk
-  test('update with ConditionExpression and RemoveKeys', async () => {
-    const client = getTestClient();
-    const service = new DynamoDBRecordService<typeof schema>(tableConfig, client as unknown as DynamoDBDocumentClient);
+  test('update with ConditionExpression ReturnValues', async () => {
+    const service = new DynamoDBRecordService<typeof schema>(tableConfig, localClient);
 
-    if (isDynamoDbLocal()) {
-      await service.batchWrite([
-        {
-          pk1: '1234',
-          resetPasswordAt: '1000',
-        },
-      ]);
-    }
-
+    await service.batchWrite([
+      {
+        pk1: '1234',
+        resetPasswordAt: '1000',
+      },
+    ]);
     await service.update(
       '1234',
       {
@@ -416,40 +173,64 @@ describe.skipIf(!isDynamoDbLocal())('DynamoDBRecordService', { tags: ['dynamodb-
         ExpressionAttributeValues: {
           ':false': false,
         },
+        ReturnValues: 'ALL_NEW',
       },
     );
 
-    // if (!isDynamoDbLocal()) {
-    //   expect(ddbMock).toHaveReceivedCommandWith(UpdateCommand, {
-    //     TableName: 'test-table',
-    //     Key: {
-    //       pk1: '1234',
-    //     },
-    //     UpdateExpression: 'SET #isAccountDeleted = :isAccountDeleted REMOVE resetPasswordAt',
-    //     ExpressionAttributeNames: {
-    //       '#isAccountDeleted': 'isAccountDeleted',
-    //     },
-    //     ExpressionAttributeValues: {
-    //       ':isAccountDeleted': true,
-    //       ':false': false,
-    //     },
-    //     ConditionExpression:
-    //       'attribute_exists(pk1) AND (attribute_not_exists(isAccountDeleted) OR isAccountDeleted = :false)',
-    //   });
-    // }
-
-    if (isDynamoDbLocal()) {
-      const results = await service.queryByPkAndValidate('1234');
-      expect(results).toEqual([{ pk1: '1234', isAccountDeleted: true }]);
-    }
+    const results = await service.queryByPkAndValidate('1234');
+    expect(results).toEqual([{ pk1: '1234', isAccountDeleted: true }]);
   });
 
-  // do not add to @dynamodb-local as it will never reach the actual database
-  // test('update with empty object', async () => {
-  //   const service = new DynamoDBRecordService<typeof schema>(tableConfig, ddbMock as unknown as DynamoDBDocumentClient);
-  //
-  //   await service.update('1234', {});
-  //
-  //   expect(ddbMock).not.toHaveReceivedAnyCommand();
-  // });
+  test('update with something ', async () => {
+    const service = new DynamoDBRecordService<typeof schema>(tableConfig, localClient);
+
+    await service.batchWrite([
+      {
+        pk1: '1234',
+        resetPasswordAt: '1000',
+      },
+    ]);
+
+
+
+  });
+
+  test('getByPkAndValidate', async () => {
+    const service = new DynamoDBRecordService<typeof schema>(tableConfig, localClient);
+    await service.batchWrite([
+      {
+        pk1: '1234',
+        resetPasswordAt: '1000',
+      },
+    ]);
+    const results = await service.getByPkAndValidate('1234');
+    expect(results).toEqual(
+      {
+        pk1: '1234',
+        resetPasswordAt: '1000',
+      },
+    );
+  });
+
+  test('getByPkAndValidate with includedKeys', async () => {
+    const service = new DynamoDBRecordService<typeof schema>(tableConfig, localClient);
+    await service.batchWrite([
+      {
+        pk1: '1234',
+        resetPasswordAt: '1000',
+      },
+    ]);
+    const results = await service.getByPkAndValidate('1234', ['resetPasswordAt']);
+    expect(results).toEqual(
+      {
+        resetPasswordAt: '1000',
+      },
+    );
+  });
+
+  test('getByPkAndValidate when no matching item', async () => {
+    const service = new DynamoDBRecordService<typeof schema>(tableConfig, localClient);
+    const results = await service.getByPkAndValidate('1234');
+    expect(results).toEqual(undefined);
+  });
 });
---
+++ src/services/test/ttl-backfill-service-docker.test.ts
@@ -0,0 +1,177 @@
+import { z } from 'zod';
+import TableConfig from '../../tables/table-config';
+import { DynamoDBTtlBackfillService } from '../ttl-backfill-service';
+import { BatchWriteCommand, ScanCommand } from '@aws-sdk/lib-dynamodb';
+import { CreateTableCommand, DeleteTableCommand, DynamoDBClient } from '@aws-sdk/client-dynamodb';
+import { DynamoDBDocumentClient } from '@aws-sdk/lib-dynamodb';
+import { TtlSource } from '../../data-types/constants';
+
+const createLocalClient = () =>
+  DynamoDBDocumentClient.from(
+    new DynamoDBClient({
+      endpoint: 'http://localhost:8000',
+      region: 'eu-west-2',
+      credentials: { accessKeyId: 'test', secretAccessKey: 'test' },
+    }),
+  );
+
+const schema = z.object({
+  accountId: z.string(),
+  createdAt: z.number(),
+  ttl: z.number().optional(),
+  ttlSource: z.enum(TtlSource).optional(),
+});
+
+const tableConfig: TableConfig<typeof schema> = {
+  tableName: 'intervention-events',
+  partitionKeyName: 'accountId',
+  schema,
+};
+
+type InterventionEvent = z.infer<typeof schema>;
+
+const isDynamoDbLocal = () => process.env['TEST_DYNAMODB_LOCAL'] === 'true';
+
+const putItems = async (localClient: DynamoDBDocumentClient, amount = 1) => {
+  const items = []
+
+ for (let i = 0; i < amount; ++i) {
+   const number = i + 1
+   const createdAt = 1500 + i * 50;
+   items.push(
+     {
+       PutRequest: {
+         Item: { accountId: `account-${number.toString()}`, createdAt },
+       },
+     })
+ }
+
+ // add an item with a ttl
+ items.push({
+   PutRequest: {
+     Item: { accountId: 'account-4', createdAt: 1250, ttl: 1000 },
+   },
+ });
+
+  await localClient.send(
+    new BatchWriteCommand({
+      RequestItems: {
+        'intervention-events': items,
+      },
+    }),
+  );
+};
+
+describe.skipIf(!isDynamoDbLocal())('DynamoDBTtlBackfillService (Docker)', { tags: ['dynamodb-local'] }, () => {
+  let localClient: DynamoDBClient;
+  beforeAll(() => {
+    localClient = createLocalClient();
+  });
+
+  beforeEach(async () => {
+    try {
+      await localClient.send(
+        new DeleteTableCommand({
+          TableName: 'intervention-events',
+        }),
+      );
+    } catch {
+      // Table may not exist on first run
+    }
+
+    await localClient.send(
+      new CreateTableCommand({
+        TableName: 'intervention-events',
+        KeySchema: [
+          { AttributeName: 'accountId', KeyType: 'HASH' },
+          { AttributeName: 'createdAt', KeyType: 'RANGE' },
+        ],
+        AttributeDefinitions: [
+          { AttributeName: 'accountId', AttributeType: 'S' },
+          { AttributeName: 'createdAt', AttributeType: 'N' },
+        ],
+        BillingMode: 'PAY_PER_REQUEST',
+      }),
+    );
+  });
+
+  test('scans for items missing ttl', async () => {
+    const service = new DynamoDBTtlBackfillService<typeof schema>(tableConfig, localClient);
+    await putItems(localClient, 3);
+
+    const result = await service.scanEventsMissingTtl({
+      windowStartMs: 1000,
+      windowEndMs: 2000,
+      limit: 50,
+    });
+
+    expect(result.scannedCount).toBe(4);
+    const sorted = result.keys.toSorted((a, b) => a.accountId.localeCompare(b.accountId));
+    expect(sorted).toEqual([
+      { accountId: 'account-1', createdAt: 1500 },
+      { accountId: 'account-2', createdAt: 1550 },
+      { accountId: 'account-3', createdAt: 1600 },
+    ]);
+  });
+
+  test('should filter between createdAt times',  async () => {
+    const service = new DynamoDBTtlBackfillService<typeof schema>(tableConfig, localClient);
+    await putItems(localClient, 3);
+
+    const result = await service.scanEventsMissingTtl({
+      windowStartMs: 1501,
+      windowEndMs: 1551,
+      limit: 50,
+    });
+
+    expect(result).toEqual({
+      keys: [
+        {
+          accountId: 'account-2',
+          createdAt: 1550,
+        },
+      ],
+      scannedCount: 4,
+    });
+  });
+
+  test('updates items that do not have a ttl', async () => {
+    const service = new DynamoDBTtlBackfillService<typeof schema>(tableConfig, localClient);
+    await putItems(localClient, 3);
+
+   const itemsWithoutTtl = [
+     { accountId: 'account-3', createdAt: 1600 },
+     { accountId: 'account-2', createdAt: 1550 },
+     { accountId: 'account-1', createdAt: 1500 },
+   ];
+
+    // update the items without ttl
+    for (const item of itemsWithoutTtl) {
+      await service.applyTtl(item, 1000);
+    }
+
+    // verify that all items have ttl
+    const result = await localClient.send(
+      new ScanCommand({
+        TableName: 'intervention-events',
+      }),
+    );
+
+    const items = result.Items as InterventionEvent[];
+
+    const sorted = items.toSorted((a, b) => a.accountId.localeCompare(b.accountId));
+    expect(sorted).toEqual([
+      { accountId: 'account-1', createdAt: 1500, ttl: 1000, ttlSource: 'BACKFILL' },
+      { accountId: 'account-2', createdAt: 1550, ttl: 1000, ttlSource: 'BACKFILL' },
+      { accountId: 'account-3', createdAt: 1600, ttl: 1000, ttlSource: 'BACKFILL' },
+      { accountId: 'account-4', createdAt: 1250, ttl: 1000 },
+    ]);
+  });
+
+  test('should return false when applyTtl is called on an item with a ttl', async () => {
+    const service = new DynamoDBTtlBackfillService<typeof schema>(tableConfig, localClient);
+    await putItems(localClient, 3);
+    const result = await service.applyTtl({ accountId: 'account-4', createdAt: 1250 }, 1000);
+    expect(result).toEqual(false);
+  });
+});
